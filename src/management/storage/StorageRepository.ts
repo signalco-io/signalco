facf58ee1da422ae1f4d7d12421632076e82882a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2020-06-26 01:09:04 +0200

    Updated API routes to match newest backend

--- src/management/storage/StorageRepository.ts
+++ src/management/storage/StorageRepository.ts
@@ -2,12 +2,12 @@ import HttpService from "../../services/HttpService";
 
 export default class StorageRepository {
   static async getTablesAsync(): Promise<Array<string>> {
-    const data = await HttpService.getAsync("storage-table-list");
+    const data = await HttpService.getAsync("system/storage/tables/list");
     return data as Array<string>;
   }
 
   static async getQueuesAsync(): Promise<Array<string>> {
-    const data = await HttpService.getAsync("storage-queues-list");
+    const data = await HttpService.getAsync("system/storage/queues/list");
     return data as Array<string>;
   }
 }
