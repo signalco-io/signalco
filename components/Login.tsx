edc89e36346b71d730a673f302e2f6ed8696062f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2020-11-30 18:29:59 +0100

    RippleIndicator

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,35 +2,41 @@ import { useAuth0 } from "@auth0/auth0-react";
 import {
   Box, Button, Grid, Typography
 } from "@material-ui/core";
+import { useEffect, useRef } from "react";
+import RippleIndicator from "./shared/indicators/RippleIndicator";
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
+  const rippleTriggerRef = useRef();
+
+  useEffect(() => {
+    const rippleIntervalClear = setInterval(() => new Date().getMilliseconds() && rippleTriggerRef.current.trigger(), 2500);
+    return () => clearInterval(rippleIntervalClear);
+  }, [])
 
   return (
     <Grid
       container
       className="login__root"
-      spacing={4}
+      wrap="nowrap"
       alignItems="center"
       direction="column"
     >
       <Grid item>
         <Grid container direction="row" spacing={2} alignItems="center">
           <Grid item>
-            <img
-              alt="Logo"
-              src="/images/icon-72x72.png"
-              width="72"
-              height="72"
-            />
+            <RippleIndicator size={72} ref={rippleTriggerRef} />
           </Grid>
           <Grid item>
             <Typography variant="h1">Signal</Typography>
           </Grid>
+          <Grid item>
+            <Box width={72} height={72}></Box>
+          </Grid>
         </Grid>
       </Grid>
       <Grid item>
-        <Box width={235}>
+        <Box sx={{ width: 315, pt: 4 }}>
           <Button
             onClick={() => loginWithRedirect()}
             variant="outlined"
---
+++ components/shared/indicators/RippleIndicator.tsx
@@ -0,0 +1,62 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { spring, TransitionMotion } from 'react-motion';
+
+const leavingSpringConfig = { stiffness: 60, damping: 15 };
+const leavingSpringBackgroundConfig = { stiffness: 140, damping: 20 };
+const willLeave = (styleCell: any) => {
+    return {
+        ...styleCell.style,
+        opacity: spring(0, leavingSpringConfig),
+        scale: spring(2, leavingSpringConfig),
+        background: spring(0, leavingSpringBackgroundConfig)
+    };
+};
+
+export interface IRippleIndicatorProps {
+    size?: number
+}
+
+const RippleIndicator = forwardRef((props : IRippleIndicatorProps, ref) => {
+    const [refresh, setRefresh] = useState<number>(0);
+    useImperativeHandle(ref, () => ({trigger() { setRefresh(Date.now()); }}));
+
+    const {size = 42} = props;
+    
+    const styles = [{
+        key: refresh,
+        style: {
+            opacity: spring(1),
+            scale: spring(0),
+            background: spring(0.4)
+        }
+    }];
+
+    return (
+        <TransitionMotion willLeave={willLeave} styles={styles}>
+            {circles =>
+                <div style={{ position: "relative", width: `${size}px`, height: `${size}px` }}>
+                    {circles.map(({ key, style: { opacity, scale, background } }) =>
+                        <div
+                            key={key}
+                            style={{
+                                width: `${size / 2}px`,
+                                height: `${size / 2}px`,
+                                top: `${size / 4}px`,
+                                left: `${size / 4}px`,
+                                borderRadius: `${size - 1}px`,
+                                position: "absolute",
+                                border: "1px solid white",
+                                backgroundColor: `rgba(255,255,255,${background})`,
+                                opacity: opacity,
+                                scale: scale,
+                                transform: `scale(${scale})`,
+                                WebkitTransform: `scale(${scale})`,
+                            }} />
+                    )}
+                </div>
+            }
+        </TransitionMotion>
+    );
+});
+
+export default RippleIndicator;
