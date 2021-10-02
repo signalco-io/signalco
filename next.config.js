ef51223fe6f515a25c47f3db803a33f34b7a06cc
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-10-02 13:03:13 +0200

    Added secure headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 const withPWA = require('next-pwa')
+const { createSecureHeaders } = require("next-secure-headers")
 
 module.exports = withPWA({
     experimental: { esmExternals: true },
     pwa: {
         dest: 'public',
         disable: process.env.NODE_ENV === 'development'
-    }
+    },
+    async headers() {
+        return [{
+          source: "/(.*)",
+          headers: createSecureHeaders({
+            contentSecurityPolicy: {
+              directives: {
+                defaultSrc: "'self'",
+                styleSrc: ["'self'", "https://stackpath.bootstrapcdn.com"],
+              },
+            },
+            forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+            referrerPolicy: "same-origin",
+            expectCT: [true,{
+                reportURI: 'https://o513630.ingest.sentry.io/api/5615895/security/?sentry_key=2a04f9a742e74740952dcebf06313840'
+            }]
+          })
+        }];
+      },
 });
