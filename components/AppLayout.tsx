bb96b6322dba83480d1d066c0fcf7353cca4909c
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-10-14 15:07:50 +0200

    Moved signalr to separate service

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,19 +1,15 @@
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import { Alert, Grid } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import HttpService from "../src/services/HttpService";
 import NavProfile from "./NavProfile";
-import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import DevicesRepository, { SignalDeviceStatePublishDto } from "../src/devices/DevicesRepository";
 import { setSentryUser } from "../src/errors/SentryUtil";
 import { useSnackbar } from 'notistack';
 import PageNotificationService from "../src/notifications/PageNotificationService";
-
-let devicesHub: HubConnection | undefined;
+import RealtimeService from '../src/realtime/realtimeService';
 
 const Layout = (props: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading, error, user, getAccessTokenSilently, loginWithRedirect } = useAuth0();
-  const [pageError] = useState<string | undefined>();
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   console.debug("AppLayout rendering");
@@ -37,6 +33,7 @@ const Layout = (props: { children: React.ReactNode }) => {
     }
   }, [loginWithRedirect, isLoading, isAuthenticated])
 
+  // Set sentry user
   useEffect(() => {
     if (isLoading || !isAuthenticated || !user) return;
 
@@ -45,81 +42,14 @@ const Layout = (props: { children: React.ReactNode }) => {
 
   // Initiate SignalR communication
   useEffect(() => {
-    if (isLoading) return;
+    if (!isLoading) {
+      RealtimeService.startAsync();
+    }
+  }, [isLoading]);
 
-    const createHubConnection = async () => {
-      if (devicesHub != null) return;
-
-      console.debug("Configuring SignalR...");
-
-      const hub = new HubConnectionBuilder()
-        .withUrl(HttpService.getApiUrl('/signalr/devices'), {
-          accessTokenFactory: async () => {
-            if (typeof HttpService.tokenFactory === 'undefined')
-              throw Error("TokenFactory not present. Unable to authorize SignalR client.");
-            return await HttpService.tokenFactory();
-          }
-        })
-        .configureLogging(LogLevel.Information)
-        .build();
-      devicesHub = hub;
-
-      const hubStartWithRetry = async (retryCount: number) => {
-        try {
-          console.debug("Connecting to SignalR...");
-
-          await hub.start();
-          hub.on("devicestate", async (state: SignalDeviceStatePublishDto) => {
-            if (typeof state.DeviceId === 'undefined' ||
-              typeof state.ChannelName === 'undefined' ||
-              typeof state.ContactName === 'undefined' ||
-              typeof state.TimeStamp === 'undefined') {
-              console.warn("Got device state with invalid values", state);
-              return;
-            }
-
-            const device = await DevicesRepository.getDeviceAsync(state.DeviceId);
-            if (typeof device !== 'undefined') {
-              device.updateState(
-                state.ChannelName,
-                state.ContactName,
-                state.ValueSerialized,
-                new Date(state.TimeStamp)
-              );
-            }
-          });
-          hub.onclose((err) => {
-            console.log("SignalR connection closed. Reconnecting with delay...");
-            console.debug("SignalR connection closes reason:", err);
-            hubStartWithRetry(0);
-          });
-          hub.onreconnecting((err) => {
-            console.log("Signalr reconnecting...");
-            console.debug("Signalr reconnection reason:", err);
-          });
-          hub.onreconnected(() => {
-            console.log("Signalr reconnected");
-            PageNotificationService.show("Realtime connection to cloud established.", "success");
-          });
-        } catch (err) {
-          const delay = retryCount + 1;
-
-          console.warn(`Failed to start SignalR hub connection. Reconnecting in ${delay}s`, err);
-          PageNotificationService.show(`Realtime connection to cloud lost. Reconnecting in ${delay}s...`, "warning");
-          setTimeout(() => {
-            hubStartWithRetry(delay);
-          }, (delay) * 1000);
-        }
-      };
-
-      hubStartWithRetry(0);
-    };
-
-    createHubConnection();
-  }, [isLoading])
-
-  if (pageError) {
-    return <Alert color="error" variant="filled">{error}</Alert>
+  // Show error if available
+  if (error) {
+    PageNotificationService.show(error.message, "error");
   }
 
   return (
---
+++ src/realtime/realtimeService.ts
@@ -0,0 +1,83 @@
+import DevicesRepository, { SignalDeviceStatePublishDto } from "../devices/DevicesRepository";
+import PageNotificationService from "../notifications/PageNotificationService";
+import HttpService from "../services/HttpService";
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+
+class RealtimeService {
+    private devicesHub?: HubConnection;
+
+private async HandleDeviceStateAsync(state: SignalDeviceStatePublishDto) {
+    if (typeof state.DeviceId === 'undefined' ||
+    typeof state.ChannelName === 'undefined' ||
+    typeof state.ContactName === 'undefined' ||
+    typeof state.TimeStamp === 'undefined') {
+    console.warn("Got device state with invalid values", state);
+    return;
+    }
+
+    const device = await DevicesRepository.getDeviceAsync(state.DeviceId);
+    if (typeof device !== 'undefined') {
+    device.updateState(
+        state.ChannelName,
+        state.ContactName,
+        state.ValueSerialized,
+        new Date(state.TimeStamp)
+    );
+    }
+}
+
+    private async hubStartWithRetryAsync(retryCount: number) {
+        try {
+            if (this.devicesHub == null) return;
+
+            console.debug("Connecting to SignalR...");
+
+            await this.devicesHub.start();
+            this.devicesHub.on("devicestate", this.HandleDeviceStateAsync);
+            this.devicesHub.onclose((err) => {
+                console.log("SignalR connection closed. Reconnecting with delay...");
+                console.debug("SignalR connection closes reason:", err);
+                this.hubStartWithRetryAsync(0);
+            });
+            this.devicesHub.onreconnecting((err) => {
+                console.log("Signalr reconnecting...");
+                console.debug("Signalr reconnection reason:", err);
+            });
+            this.devicesHub.onreconnected(() => {
+                console.log("Signalr reconnected");
+                PageNotificationService.show("Realtime connection to cloud established.", "success");
+            });
+            } catch (err) {
+            const delay = retryCount + 1;
+
+            console.warn(`Failed to start SignalR hub connection. Reconnecting in ${delay}s`, err);
+            PageNotificationService.show(`Realtime connection to cloud lost. Reconnecting in ${delay}s...`, "warning");
+            setTimeout(() => {
+                this.hubStartWithRetryAsync(delay);
+            }, (delay) * 1000);
+        }
+    };
+
+    async startAsync() {
+        if (this.devicesHub != null) return;
+  
+        console.debug("Configuring SignalR...");
+  
+        this.devicesHub = new HubConnectionBuilder()
+          .withUrl(HttpService.getApiUrl('/signalr/devices'), {
+            accessTokenFactory: async () => {
+              if (typeof HttpService.tokenFactory === 'undefined')
+                throw Error("TokenFactory not present. Unable to authorize SignalR client.");
+              return await HttpService.tokenFactory();
+            }
+          })
+          .configureLogging(LogLevel.Information)
+          .build();
+    
+        this.hubStartWithRetryAsync(0);
+    };
+}
+
+const service = new RealtimeService();
+
+export default service;
