737389666ab2c802ba03caca1f9f84d6f21c3dce
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-10-17 12:45:04 +0200

    Added other visuals, animated visuals

--- components/devices/parts/WidgetState.tsx
+++ components/devices/parts/WidgetState.tsx
@@ -1,7 +1,13 @@
+import { ArrowDownward, ArrowUpward, Stop } from '@mui/icons-material';
 import BatteryCharging20OutlinedIcon from '@mui/icons-material/BatteryCharging20Outlined';
-import { Grid, Stack, Typography, Button } from "@mui/material";
-import React from "react";
+import { Grid, Stack, Typography, Button, ButtonBase } from "@mui/material";
+import { observer } from 'mobx-react-lite';
+import React, { useState, useEffect } from "react";
+import { IDeviceModel } from '../../../src/devices/Device';
+import DevicesRepository from '../../../src/devices/DevicesRepository';
+import LightBulbVisual from '../../icons/LightBulbVisual';
 import TvVisual from "../../icons/TvVisual";
+import WindowVisual from '../../icons/WindowVisual';
 import WidgetCard from './WidgetCard';
 
 export const WidgetVacuum = (props: { config?: any }) => {
@@ -28,23 +34,28 @@ export const WidgetVacuum = (props: { config?: any }) => {
 export const WidgetShades = (props: { config: any }) => {
     const width = 4;
     const height = 2;
-    const label = "Living room shades";
-    const shadePerc = 20;
-    const state = shadePerc > 0;
+    const label = props.config?.label || '';
+
+    // TODO: Calc from source value
+    const shadePerc = 1;
+
+    const state = shadePerc < 1;
 
     return (
         <WidgetCard width={width} height={height} state={state}>
-            <Grid container>
-                <Grid item>
-                    <Stack>
-                        <TvVisual state={state} theme="dark" size={52} />
-                        <Typography>{label}</Typography>
+            <Grid container wrap="nowrap" sx={{ height: '100%' }}>
+                <Grid item xs={6}>
+                    <Stack sx={{ height: '100%', pl: 2.5, pr: 1.5, py: 2 }} justifyContent="space-between">
+                        <WindowVisual shadePerc={shadePerc} theme="dark" size={68} />
+                        <Typography fontWeight="light">{label}</Typography>
                     </Stack>
                 </Grid>
-                <Grid item>
-                    <Button />
-                    <Button />
-                    <Button />
+                <Grid item xs={6} sx={{ background: '#121212', borderLeft: "1px solid rgba(255,255,255,0.12)" }}>
+                    <Stack sx={{ height: '100%' }} justifyContent="stretch">
+                        <Button sx={{ flexGrow: 1, borderBottom: '1px solid rgba(255,255,255,0.12)' }}><ArrowUpward /></Button>
+                        <Button sx={{ flexGrow: 1 }}><Stop /></Button>
+                        <Button sx={{ flexGrow: 1, borderTop: '1px solid rgba(255,255,255,0.12)' }}><ArrowDownward /></Button>
+                    </Stack>
                 </Grid>
             </Grid>
         </WidgetCard>
@@ -52,19 +63,34 @@ export const WidgetShades = (props: { config: any }) => {
 };
 
 const WidgetState = (props: { config: any }) => {
+    const [device, setDevice] = useState<IDeviceModel | undefined>(undefined);
+
     const width = 2;
     const height = 2;
-    const state = false;
-    const label = "Living room TV"
+
+    // Calc state from source value
+    const contactState = device?.getState({ channelName: 'philipshue', contactName: 'on', deviceId: device.id });
+    const state = contactState?.valueSerialized === 'true';
+
+    useEffect(() => {
+        (async () => {
+            setDevice(await DevicesRepository.getDeviceAsync('f9983f10-4a69-4f5a-b113-32b541e3c536'));
+        })();
+    }, []);
+
+    const label = props.config?.label || device?.alias || '';
+    const Visual = props.config?.visual === 'tv' ? TvVisual : LightBulbVisual;
 
     return (
         <WidgetCard width={width} height={height} state={state}>
-            <Stack sx={{ p: 3 }}>
-                <TvVisual state={state} theme="dark" size={52} />
-                <Typography fontWeight="light">{label}</Typography>
-            </Stack>
+            <ButtonBase sx={{ height: '100%', width: '100%', justifyContent: 'flex-start' }} >
+                <Stack sx={{ height: '100%', pl: 2.5, pr: 1.5, py: 2 }} justifyContent="space-evenly">
+                    <Visual state={state} theme="dark" size={68} />
+                    <Typography sx={{ display: 'flex' }} fontWeight="light">{label}</Typography>
+                </Stack>
+            </ButtonBase>
         </WidgetCard>
     );
 };
 
-export default WidgetState;
+export default observer(WidgetState);
--- components/icons/TvVisual.tsx
+++ components/icons/TvVisual.tsx
@@ -1,7 +1,40 @@
-import Image from 'next/image';
+import styles from './TvVisual.module.scss';
 
 const TvVisual = (props: { state: boolean, theme: "dark" | "light", size: number }) => {
-    return <Image src="/assets/visuals/tvVisual.svg" width={props.size} height={props.size} alt="TV" />;
+    const isActive = props.state;
+    const screenColor = isActive ? "#378DBD" : "#333";
+    const edgeColor = props.theme === 'dark' ? "#fff" : "#000";
+    const shadowColor = props.theme === 'dark' ? "#000" : "#fff";
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className={styles.root} width={props.size * 1.6} height={props.size * 1.6} fill="none" viewBox="0 0 103 77">
+            <g className="Theme=Dark, State=On">
+                <path fill={edgeColor} d="M19 14h65v40.524H19z" className="Edge" />
+                <path fill={shadowColor} fillOpacity=".12" d="M19 14h33.621v40.524H19z" className="EdgeShadow" />
+                <path fill={edgeColor} d="M39.172 54.524h24.655v3.286H39.172z" className="BaseStand" />
+                <path fill={shadowColor} fillOpacity=".12" d="M39.172 54.524H52.62v3.286H39.172z" className="BaseStandShadow" />
+                <path fill={edgeColor} d="M32.448 57.809h38.103v2.19H32.448z" className="Base" />
+                <path fill={shadowColor} fillOpacity=".12" d="M32.448 57.809H52.62v2.19H32.448z" className="BaseShadow" />
+                <g className="Screen" filter={isActive ? "url(#tvvisualfilter0_d)" : ''}>
+                    <path fill={screenColor} d="M22.362 18.381h58.276v31.762H22.362z" />
+                </g>
+                <path fill="#121212" floodOpacity=".12" opacity={isActive ? "0" : "1"} d="M22.362 18.381h30.259v31.762H22.362z" className="ScreenShadow" />
+            </g>
+            <defs>
+                <filter id="tvvisualfilter0_d" width="102.276" height="75.762" x=".362" y=".381" className="tvvisualfilter0_d" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+                    <feMorphology in="SourceAlpha" operator="dilate" radius="2" result="effect1_dropShadow" />
+                    <feOffset dy="4" />
+                    <feGaussianBlur stdDeviation="10" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix values="0 0 0 0 0.553125 0 0 0 0 0.880083 0 0 0 0 0.983333 0 0 0 0.5 0" />
+                    <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
+                    <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+                </filter>
+            </defs>
+        </svg>
+    );
 }
 
 export default TvVisual;
