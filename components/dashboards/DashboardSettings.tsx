64c127c8238dfc76170b0fe5d2ca4054ecf03b2a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2021-11-27 01:27:01 +0100

    Moved confirm delete dialog to shared components

--- components/dashboards/DashboardSettings.tsx
+++ components/dashboards/DashboardSettings.tsx
@@ -1,6 +1,7 @@
-import { Breakpoint, Button, Stack, TextField, Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import ConfigurationDialog from "../shared/dialog/ConfigurationDialog";
+import ConfirmDeleteButton from "../shared/dialog/ConfirmDeleteButton";
 import { IDashboard } from "./Dashboards";
 
 interface IDashboardSettingsProps {
@@ -10,63 +11,6 @@ interface IDashboardSettingsProps {
     onChange: (dashboard: IDashboard) => void,
     onDeleteConfirmed: () => void
 }
-
-interface IConfirmDeleteDialogProps {
-    isOpen: boolean,
-    title: React.ReactNode,
-    expectedConfirmText: string,
-    onClose: () => void,
-    onConfirm: () => void,
-    maxWidth?: false | undefined | Breakpoint,
-}
-
-interface IConfirmDeleteButtonDialogProps {
-    title: React.ReactNode,
-    expectedConfirmText: string,
-    onConfirm: () => void,
-    maxWidth?: false | undefined | Breakpoint,
-}
-
-interface IConfirmDeleteButtonProps extends IConfirmDeleteButtonDialogProps {
-    buttonLabel: string
-}
-
-const ConfirmDeleteButton = (props: IConfirmDeleteButtonProps) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleConfirm = () => {
-        setIsOpen(false);
-        props.onConfirm();
-    }
-
-    return (
-        <>
-            <Button variant="outlined" color="error" onClick={() => setIsOpen(true)}>{props.buttonLabel}</Button>
-            <ConfirmDeleteDialog isOpen={isOpen} onClose={() => setIsOpen(false)} {...props} onConfirm={handleConfirm} />
-        </>
-    )
-};
-
-const ConfirmDeleteDialog = (props: IConfirmDeleteDialogProps) => {
-    const { isOpen, title, expectedConfirmText, onClose, onConfirm, maxWidth } = props;
-    const [confirmText, setConfirmText] = useState('');
-
-    return (
-        <ConfigurationDialog
-            title={title}
-            isOpen={isOpen}
-            onClose={onClose}
-            maxWidth={maxWidth}>
-            <Stack spacing={4}>
-                <Typography>Deleted item can not be restored. Please confirm you are deleting correct item by entering &quot;<code>{expectedConfirmText}</code>&quot; bellow.</Typography>
-                <TextField label="Confirm" onChange={(e) => setConfirmText(e.target.value)} />
-                <Button variant="contained" color="error" disabled={confirmText !== expectedConfirmText} onClick={onConfirm}>
-                    Delete &quot;{confirmText}&quot;
-                </Button>
-            </Stack>
-        </ConfigurationDialog>
-    );
-};
 
 const DashboardSettings = (props: IDashboardSettingsProps) => {
     const { isOpen, dashboard, onClose, onChange, onDeleteConfirmed } = props;
---
+++ components/shared/dialog/ConfirmDeleteButton.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@mui/material";
+import { Breakpoint } from "@mui/system";
+import React, { useState } from "react";
+import ConfirmDeleteDialog from "./ConfirmDeleteDialog";
+
+export interface IConfirmDeleteButtonDialogProps {
+    title: React.ReactNode,
+    expectedConfirmText: string,
+    onConfirm: () => void,
+    maxWidth?: false | undefined | Breakpoint,
+}
+
+export interface IConfirmDeleteButtonProps extends IConfirmDeleteButtonDialogProps {
+    buttonLabel: string
+}
+
+const ConfirmDeleteButton = (props: IConfirmDeleteButtonProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleConfirm = () => {
+        setIsOpen(false);
+        props.onConfirm();
+    }
+
+    return (
+        <>
+            <Button variant="outlined" color="error" onClick={() => setIsOpen(true)}>{props.buttonLabel}</Button>
+            <ConfirmDeleteDialog isOpen={isOpen} onClose={() => setIsOpen(false)} {...props} onConfirm={handleConfirm} />
+        </>
+    )
+};
+
+export default ConfirmDeleteButton;
