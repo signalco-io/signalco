639a0ef9529f9d34f9542a6d8cc8ecb76cab9b01
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-01-13 22:19:31 +0100

    Added storybook

---
+++ components/stories/Button.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Button, Container } from '@mui/material';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Button',
+  component: Button,
+  decorators: [
+    Story => (
+      <Container maxWidth='lg'>
+        <Story />
+      </Container>
+    )
+  ]
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} variant="contained" />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  color: "primary",
+  children: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Button',
+  color: 'secondary'
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  children: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  children: 'Button',
+};
