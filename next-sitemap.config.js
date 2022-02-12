dd36c069fc54a5f6d637c77849e8507dd65892cc
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-02-12 21:19:12 +0100

    Added sitemap
    Closes #278

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.signalco.io',
+    generateRobotsTxt: true,
+    exclude: ["/app/*", "/login-return"]
+    // ...other options
+}
