fda092fbf6b238c6ec61100572df2e1f9a374c85
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-03-13 13:59:10 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import theme from "../src/theme";
