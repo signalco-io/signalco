6ae89059655fc44c5699d7d921cb4a1a20892649
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-03-23 15:55:04 +0100

    Fixed postinstall script for windows

---
+++ tools/post-install.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var fs = require("fs-extra");
+
+fs.copy('./node_modules/monaco-editor/min/vs', './public/vs', function (err) {
+    if (err) {
+        console.log('An error occured while copying the folder.');
+        return console.error(err);
+    }
+    console.log('Copy completed!');
+});
