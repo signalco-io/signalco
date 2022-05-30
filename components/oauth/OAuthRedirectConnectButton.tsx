dc20c6fdb8dd56bbef68dda924b8ba84863b0264
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-05-31 00:20:26 +0200

    Added slack channel pages and OAuth redirect button

---
+++ components/oauth/OAuthRedirectConnectButton.tsx
@@ -0,0 +1,35 @@
+import { LoadingButton } from '@mui/lab';
+import { NoSsr } from '@mui/material';
+import { useEffect, useState } from '@storybook/addons';
+import { useRouter } from 'next/router';
+
+export default function OAuthRedirectConnectButton(props: { label: string; initiateUrl: string; queryParamName: string; onCode: (code: string) => Promise<void>; }) {
+    const { label, initiateUrl, queryParamName, onCode } = props;
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    const queryParamValue = router.query ? router.query[queryParamName]?.toString() : undefined;
+
+    useEffect(() => {
+        async function requestAccess() {
+            if (!queryParamValue)
+                return;
+
+            // TODO: Show error or success
+            try {
+                setIsLoading(true);
+                await onCode(queryParamValue);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        requestAccess();
+    }, [onCode, queryParamValue]);
+
+    // TODO: Show "Connected" if already connected
+    return (
+        <NoSsr>
+            <LoadingButton loading={!router.isReady || isLoading} variant="outlined" href={initiateUrl}>{label}</LoadingButton>
+        </NoSsr>
+    );
+}
