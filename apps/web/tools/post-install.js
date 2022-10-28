7f48c3859535d9105764524797d3fa01569892d9
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-10-28 14:03:19 +0200

    chore(Build): Fixed post install monaco script

--- apps/web/tools/post-install.js
+++ apps/web/tools/post-install.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs-extra');
 console.log('Copy monaco-editor resources...')
-fs.copy('./node_modules/monaco-editor/min/vs', './public/vs', function (err) {
+fs.copy('../../node_modules/monaco-editor/min/vs', './public/vs', function (err) {
     if (err) {
         console.log('An error occured while copying the folder.');
         return console.error(err);
