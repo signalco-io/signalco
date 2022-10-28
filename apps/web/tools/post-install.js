392594f1073724c7e4ff747305eaa173d2328930
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-10-28 13:36:23 +0200

    feat(DX): Added turborepo

--- apps/web/tools/post-install.js
+++ apps/web/tools/post-install.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var fs = require('fs-extra');
-
+console.log('Copy monaco-editor resources...')
 fs.copy('./node_modules/monaco-editor/min/vs', './public/vs', function (err) {
     if (err) {
         console.log('An error occured while copying the folder.');
