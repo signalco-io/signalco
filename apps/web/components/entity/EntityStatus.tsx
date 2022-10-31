a0dbd3e79815e19d5dfda1bfe0392962ea2cbf5a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-10-31 13:20:02 +0100

    fix(Entity): Status indicator changes

--- apps/web/components/entity/EntityStatus.tsx
+++ apps/web/components/entity/EntityStatus.tsx
@@ -3,26 +3,43 @@ import DateTimeProvider from 'src/services/DateTimeProvider';
 import IEntityDetails from 'src/entity/IEntityDetails';
 import { entityInError, entityLastActivity } from 'src/entity/EntityHelper';
 import DotIndicator from 'components/shared/indicators/DotIndicator';
+import { useMemo } from 'react';
+
+export function useEntityStatus(entity: IEntityDetails | undefined) {
+    const hasStatus = entity && entity.type === 1;
+    const isOffline = useMemo(() => hasStatus ? entityInError(entity) : null, [entity, hasStatus]);
+    const lastActivity = useMemo(() => hasStatus ? entityLastActivity(entity) : null, [entity, hasStatus]);
+    const isStale = useMemo(() => (hasStatus && lastActivity) ? DateTimeProvider.now().getTime() - lastActivity.getTime() > 24 * 60 * 60 * 1000 : null, [lastActivity, hasStatus]);
+
+    const result = useMemo(() => ({
+        hasStatus,
+        isOffline,
+        lastActivity,
+        isStale
+    }), [isOffline, lastActivity, isStale]);
+
+    return result;
+}
+
+type EntityStatusColors = 'success' | 'warning' | 'danger' | 'neutral';
 
 export default function EntityStatus(props: { entity: IEntityDetails | undefined }) {
-    const { entity } = props;
+    const { hasStatus, isOffline, lastActivity, isStale } = useEntityStatus(props.entity);
+    if (!hasStatus)
+        return null;
 
-    if (!entity || entity.type !== 1) return null;
-
-    const isOffline = entityInError(entity);
-    let statusColor: 'success' | 'warning' | 'danger' | 'neutral' = 'success';
+    let statusColor: EntityStatusColors  = 'success';
     let content: React.ReactNode | undefined = undefined;
     if (isOffline) {
         statusColor = 'danger';
         content = <BoltIcon fontSize="small" />;
     }
-    else if (typeof isOffline === 'undefined') {
-        const lastActivity = entityLastActivity(entity);
+    else if (isOffline == null) {
         if (!lastActivity) {
             statusColor = 'neutral';
         } else {
-            const isStale = DateTimeProvider.now().getTime() - lastActivity.getTime() > 24 * 60 * 60 * 1000;
-            if (isStale) statusColor = 'warning';
+            if (isStale)
+                statusColor = 'warning';
         }
     }
 
