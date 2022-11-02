cf9e4e59f9516fb85fb185c45f7011c6f662e4d0
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-11-02 15:29:27 +0100

    chore(DX): Build fixes

--- apps/web/src/processes/ProcessesRepository.ts
+++ apps/web/src/processes/ProcessesRepository.ts
@@ -1,10 +1,10 @@
 import IEntityDetails from 'src/entity/IEntityDetails';
 import { entitiesAsync, entityAsync } from 'src/entity/EntityRepository';
-import ContactRepository from 'src/contacts/ContactRepository';
+import { setAsync } from 'src/contacts/ContactRepository';
 
 export default class ProcessesRepository {
     static async saveProcessConfigurationAsync(id: string, configurationSerialized: string) {
-        await ContactRepository.setAsync({entityId: id, channelName: 'config', contactName: 'configuration'}, configurationSerialized);
+        await setAsync({entityId: id, channelName: 'config', contactName: 'configuration'}, configurationSerialized);
     }
 
     static async getProcessAsync(id: string): Promise<IEntityDetails | undefined> {
