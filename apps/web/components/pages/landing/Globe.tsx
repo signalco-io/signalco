0e1230cf8b09512ea6b57d5124a1bc94749e2e50
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-11-02 15:20:40 +0100

    chore(DX): Fixed TS warnings in Globe

--- apps/web/components/pages/landing/Globe.tsx
+++ apps/web/components/pages/landing/Globe.tsx
@@ -4,20 +4,20 @@ import { useColorScheme } from '@mui/joy/styles';
 import useWindowWidth from '../../../src/hooks/useWindowWidth';
 
 function Globe() {
-    const canvasRef = useRef(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const rectWidth = useWindowWidth();
     const width = Math.min(1100, rectWidth ?? 0);
     const height = width;
     const { colorScheme } = useColorScheme();
     const isDark = colorScheme === 'dark';
 
-    const glow = useMemo(() => isDark ? [0.1,0.1,0.1] : [1, 1, 1], [isDark]);
-    const base = useMemo(() => isDark ? [0.4,0.4,0.4] : [0.9,0.9,0.9], [isDark]);
+    const glow: [number, number, number] = useMemo(() => isDark ? [0.1,0.1,0.1] : [1, 1, 1], [isDark]);
+    const base: [number, number, number] = useMemo(() => isDark ? [0.4,0.4,0.4] : [0.9,0.9,0.9], [isDark]);
 
     useEffect(() => {
         let phi = 4.1;
 
-        if (!width || !height)
+        if (!width || !height || canvasRef.current == null)
             return;
 
         const globe = createGlobe(canvasRef.current, {
@@ -34,7 +34,7 @@ function Globe() {
             markerColor: [0.1, 0.8, 1],
             glowColor: glow,
             markers: [],
-            onRender: (state: { phi: number }) => {
+            onRender: (state: Record<string, any>) => {
                 state.phi = phi;
                 phi += 0.0005;
             }
