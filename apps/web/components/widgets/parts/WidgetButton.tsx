35cffdca20fa6d351ec8ec1eff4b8bbf464e95dd
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-11-11 16:56:16 +0100

    fix(Widgets): Button widget targets now take multiple targets

--- apps/web/components/widgets/parts/WidgetButton.tsx
+++ apps/web/components/widgets/parts/WidgetButton.tsx
@@ -1,26 +1,36 @@
 import { IconButton } from '@mui/joy';
+import IContact from 'src/contacts/IContact';
 import Icon from 'components/shared/Icon';
 import { StateAction, executeStateActionsAsync } from './WidgetState';
 import { WidgetSharedProps } from '../Widget';
-import { DefaultHeight, DefaultTargetWithValue, DefaultWidth } from '../../../src/widgets/WidgetConfigurationOptions';
+import { DefaultHeight, DefaultTargetWithValueMultiple, DefaultWidth } from '../../../src/widgets/WidgetConfigurationOptions';
 import IWidgetConfigurationOption from '../../../src/widgets/IWidgetConfigurationOption';
 import useWidgetOptions from '../../../src/hooks/widgets/useWidgetOptions';
 
-const stateOptions: IWidgetConfigurationOption<any>[] = [
+type ConfigProps = {
+    icon: string | undefined;
+    label: string | undefined;
+    target: IContact[];
+    polygonApiKey: string;
+    rows: number;
+    columns: number;
+}
+
+const stateOptions: IWidgetConfigurationOption<ConfigProps>[] = [
     { label: 'Icon', name: 'icon', type: 'string', optional: true },
-    DefaultTargetWithValue,
+    { label: 'Label', name: 'label', type: 'string', optional: true },
+    DefaultTargetWithValueMultiple,
     DefaultHeight(1),
     DefaultWidth(1)
 ];
 
-export default function WidgetButton(props: WidgetSharedProps<any>) {
+export default function WidgetButton(props: WidgetSharedProps<ConfigProps>) {
     const { config } = props;
 
     const icon = config?.icon ?? 'adjust';
 
     const handleActionRequest = () => {
-        const targetArray = Array.isArray(config.target) ? config.target : [config.target];
-        executeStateActionsAsync((targetArray as StateAction[]).map(d => ({
+        executeStateActionsAsync((config?.target as StateAction[]).map(d => ({
             entityId: d.entityId,
             channelName: d.channelName,
             contactName: d.contactName,
