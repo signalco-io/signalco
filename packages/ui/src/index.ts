3378cf4642d3897cfd0a77e138090c82e87c4a6c
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-12-07 00:24:16 +0100

    chore(CI): Fixed in-workspace import versions

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -81,7 +81,7 @@ export {
     TextField
 } from "@mui/joy";
 
-export { useColorScheme } from '@mui/joy/styles';
+export { useColorScheme } from '@mui/joy';
 
 // TODO: Remove as we should use direct joy types
 export type { ButtonProps, ColorPaletteProp, SupportedColorScheme, TextFieldProps, Theme } from '@mui/joy';
