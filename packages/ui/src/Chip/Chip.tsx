33863fdc5c667771b360051ec7319f0396671ed3
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-12-11 12:35:47 +0100

    Update Chip.tsx

--- packages/ui/src/Chip/Chip.tsx
+++ packages/ui/src/Chip/Chip.tsx
@@ -2,7 +2,7 @@ import { type MouseEventHandler, type ReactNode } from 'react';
 import { Chip as JoiChip } from '@mui/joy';
 import { type ChildrenProps } from '../sharedTypes';
 
-export interface ChipProps extends ChildrenProps {
+export type ChipProps = ChildrenProps & {
     color?: "primary" | "neutral" | "danger" | "info" | "success" | "warning";
     size?: 'sm' | 'md' | 'lg'
     onClick?: MouseEventHandler<HTMLButtonElement>
