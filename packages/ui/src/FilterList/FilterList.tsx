83d8f5641cf8981a8be8c43d711704bc6d5b95a5
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-01-13 23:26:40 +0100

    feat(UI): Added animations to FilterList

--- packages/ui/src/FilterList/FilterList.tsx
+++ packages/ui/src/FilterList/FilterList.tsx
@@ -7,6 +7,8 @@ import { Box } from '@mui/system';
 import Row from '../Row';
 import Stack from '../Stack';
 import Grow from '../Grow';
+import Collapse from '../Collapse';
+import Fade from '../Fade';
 
 export interface FilterListItem {
     id: string;
@@ -21,6 +23,24 @@ export interface FilterListProps {
     multiple?: boolean;
     onSelected?: (selectedId: string | undefined) => void;
     onSelectedMultiple?: (selectedIds: string[]) => void;
+}
+
+function FilterItem({ item, checked, onToggle }: { item: FilterListItem, checked: string[], onToggle: (id: string) => void }) {
+    return (
+        <Checkbox
+            key={item.id}
+            label={(
+                <Row style={{ padding: 12 }}>
+                    <Typography sx={{ flexGrow: 1 }}>{item.label}</Typography>
+                    <Grow appear={checked.indexOf(item.id) >= 0}>
+                        <Check />
+                    </Grow>
+                </Row>
+            )}
+            checked={checked.indexOf(item.id) >= 0}
+            onChange={() => onToggle(item.id)}
+            disableIcon />
+    );
 }
 
 export default function FilterList(props: FilterListProps) {
@@ -81,30 +101,22 @@ export default function FilterList(props: FilterListProps) {
                     }} />
             </Box>
             <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-                <Row spacing={1}>
-                    <Typography gutterBottom level="h5">{header}</Typography>
-                    {(!isShowMore && shouldTruncate) && <Typography level="body2">({items.length - truncate} more)</Typography>}
-                </Row>
+                <Typography level="h5" gutterBottom>{header}</Typography>
                 <Stack>
-                    {items.slice(0, isShowMore ? items.length : truncate).map(item => (
-                        <Checkbox
-                            key={item.id}
-                            label={(
-                                <Row style={{ padding: 12 }}>
-                                    <Typography sx={{ flexGrow: 1 }}>{item.label}</Typography>
-                                    <Grow appear={checked.indexOf(item.id) >= 0}>
-                                        <Check />
-                                    </Grow>
-                                </Row>
-                            )}
-                            checked={checked.indexOf(item.id) >= 0}
-                            onChange={() => handleToggle(item.id)}
-                            disableIcon />
+                    {items.slice(0, truncate).map(item => (
+                        <FilterItem item={item} checked={checked} onToggle={handleToggle} />
                     ))}
                 </Stack>
-                {(!isShowMore && shouldTruncate) && (
-                    <Button fullWidth variant="plain" startDecorator={<ExpandDown />} onClick={handleToggleShowMore}>Show all</Button>
-                )}
+                <Collapse appear={isShowMore}>
+                    <Stack>
+                        {items.slice(truncate).map(item => (
+                            <FilterItem item={item} checked={checked} onToggle={handleToggle} />
+                        ))}
+                    </Stack>
+                </Collapse>
+                <Fade appear={!isShowMore && shouldTruncate}>
+                    <Button fullWidth variant="plain" startDecorator={<ExpandDown />} onClick={handleToggleShowMore}>Show all {!!truncate && `(${items.length - truncate} more)`}</Button>
+                </Fade>
                 {(checked.length > 1 && (
                     <Button fullWidth startDecorator={<Close />} variant="plain" size="sm" onClick={handleClearSelection}>Clear selection</Button>
                 ))}
