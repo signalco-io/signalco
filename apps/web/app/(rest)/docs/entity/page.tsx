b0e02df18131af5b8d6e6fd49120a6beecfe569d
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-02-17 10:08:50 +0100

    fix(Web): Added missing docs of special entity contacts

---
+++ apps/web/app/(rest)/docs/entity/page.tsx
@@ -0,0 +1,13 @@
+import Stack from "@signalco/ui/dist/Stack";
+
+export default function EntityDocsPage() {
+    return (
+        <Stack>
+            <h3>Special contacts</h3>
+            <p>offline</p>
+            <p>battery</p>
+            <p>linkquality (planned)</p>
+            <p>visit or visit-*</p>
+        </Stack>
+    );
+}
