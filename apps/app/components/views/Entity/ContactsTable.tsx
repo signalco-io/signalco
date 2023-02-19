0e954ae3b38e8f2d30a19daefd3737909585f8ed
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-02-19 19:04:14 +0100

    Update ContactsTable.tsx

--- apps/app/components/views/Entity/ContactsTable.tsx
+++ apps/app/components/views/Entity/ContactsTable.tsx
@@ -71,6 +71,7 @@ function ObjectVisualizer(props: { name: string, value: ParsedJson, defaultOpen?
                         //     - GUID/UUID
                         //     - boolean
                         //     - ability to unset value
+                        <CopyToClipboardInput size="sm" variant="outlined" value={value?.toString()} />
                     )}
                 </Row>
             )}>
