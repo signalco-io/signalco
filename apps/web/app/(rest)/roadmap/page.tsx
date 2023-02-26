b15b5f4a1a8f719cc3829a0df383af5badf1a295
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-02-26 02:54:17 +0100

    chore(Web): Using read data for roadmap

--- apps/web/app/(rest)/roadmap/page.tsx
+++ apps/web/app/(rest)/roadmap/page.tsx
@@ -1,106 +1,128 @@
 'use client';
 
+import { Octokit } from 'octokit';
+import { Up } from '@signalco/ui-icons';
 import Stack from '@signalco/ui/dist/Stack';
 import Row from '@signalco/ui/dist/Row';
+import { Button, Card, ChildrenProps, Chip, Loadable, LoadableProps, NoDataPlaceholder, Typography } from '@signalco/ui';
 import { camelToSentenceCase, orderBy } from '@signalco/js';
-import { Button, Card, Typography } from '@signalco/ui';
-import { Up } from '@signalco/ui-icons';
+import { useLoadAndError } from '@signalco/hooks';
+
+type RoadmapItemStatus = 'triage' | 'planned' | 'inQueue' | 'inProgress' | 'completed';
 
 type RoadmapItem = {
     title: string;
-    status: 'triage' | 'planned' | 'in-queue' | 'in-progress' | 'completed';
-    scope: 'app' | 'channels';
+    status: RoadmapItemStatus;
+    scope: string;
     votes?: number | undefined;
 }
 
-const items: RoadmapItem[] = [
-    {
-        title: 'Improve Accessibility',
-        status: 'planned',
-        scope: 'app'
-    },
-    {
-        title: 'Nordigen',
-        status: 'triage',
-        scope: 'channels'
-    },
-    {
-        title: 'Mi Home',
-        status: 'planned',
-        scope: 'channels'
-    },
-    {
-        title: 'Bambu Lab',
-        status: 'in-queue',
-        scope: 'channels'
-    },
-    {
-        title: 'Zigbee2MQTT',
-        status: 'in-progress',
-        scope: 'channels',
-        votes: 13
-    },
-    {
-        title: 'Slack',
-        status: 'in-progress',
-        scope: 'channels',
-        votes: 2
-    },
-    {
-        title: 'Sun (time of day)',
-        status: 'in-progress',
-        scope: 'channels',
-        votes: 5
-    }
-];
+const statusOrderedList: RoadmapItemStatus[] = ['inProgress', 'inQueue', 'planned', 'triage', 'completed'];
 
 function RoadmapItem({ item }: { item: RoadmapItem }) {
     return (
-        <Card key={item.title} sx={{ minWidth: '220px' }}>
-            <Row spacing={2} alignItems="start">
+        <Card key={item.title} sx={{ width: '280px' }}>
+            <Row spacing={2} alignItems="start" style={{ height: '100%' }}>
                 <Button size="sm">
                     <Stack alignItems="center" spacing={1} style={{ paddingTop: 4 }}>
                         <Up />
                         <span style={{ paddingBottom: 16 }}>{item.votes ?? 'Vote'}</span>
                     </Stack>
                 </Button>
-                <Stack>
-                    <div>{item.title}</div>
-                    <Typography level="body2"></Typography>
+                <Stack spacing={1} justifyContent="space-between" style={{ flexGrow: 1, overflow: 'hidden', height: '100%' }}>
+                    <Typography level="body2">{item.title}</Typography>
+                    <Row justifyContent="end">
+                        <Chip>{camelToSentenceCase(item.scope)}</Chip>
+                    </Row>
                 </Stack>
             </Row>
         </Card>
     );
 }
 
+async function getRoadmapItemsAsync() {
+    const octokit = new Octokit();
+
+    const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
+        owner: 'signalco-io',
+        repo: 'signalco',
+        per_page: 50,
+    });
+
+    const items: RoadmapItem[] = [];
+    for await (const { data: issues } of iterator) {
+        for (const issue of issues) {
+            const labelNames = issue.labels.map(l => typeof l === 'string' ? l : l.name);
+
+            // Ignore issues that are not features or enhancements
+            if (!labelNames.includes('feature') &&
+                !labelNames.includes('enhancement')) {
+                continue;
+            }
+
+            const title = issue.title;
+            const scope = labelNames.find(label => label?.startsWith('area:'))?.replace('area:', '');
+            const isApproved = !labelNames.includes('needs-triage');
+            const inQueue = !!issue.milestone;
+            const inProgress = !!issue.assignee;
+            const isComplete = issue.state === 'closed';
+            const votes = issue.reactions?.['+1'] ?? 0;
+            let status: RoadmapItemStatus = 'triage';
+            if (isApproved) {
+                status = 'planned';
+            }
+            if (inQueue) {
+                status = 'inQueue';
+            }
+            if (inProgress) {
+                status = 'inProgress';
+            }
+            if (isComplete) {
+                status = 'completed';
+            }
+
+            if (title && scope) {
+                items.push({
+                    title,
+                    scope,
+                    status,
+                    votes
+                });
+            }
+        }
+    }
+    return items;
+}
+
+function ItemsWrapper({ children, noItemsPlaceholder, isLoading, error, loadingLabel }: ChildrenProps & LoadableProps & { noItemsPlaceholder: string }) {
+    return (
+        <Loadable isLoading={isLoading} error={error} loadingLabel={loadingLabel}>
+            {!children || !Array.isArray(children) || children.length === 0 ? (
+                <NoDataPlaceholder content={noItemsPlaceholder} />
+            ) : (
+                <>{children}</>
+            )}
+        </Loadable>
+    )
+}
+
 export default function RoadmapPage() {
-    const scopes = [...new Set(items.map(item => item.scope))];
+    const { item: items, isLoading, error } = useLoadAndError(getRoadmapItemsAsync);
 
     return (
-        <div>
-            <Stack spacing={8}>
-                {scopes.map(scope => (
-                    <Row key={scope} spacing={2}>
-                        <div style={{
-
-                        }}>
-                            <h2>{camelToSentenceCase(scope)}</h2>
-                        </div>
-                        <Stack spacing={1}>
-                            {[...new Set(items.filter(item => item.scope === scope).map(item => item.status))].map(status => (
-                                <>
-                                    <div>{status}</div>
-                                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8 }}>
-                                        {orderBy(items.filter(item => item.scope === scope && item.status === status), (a, b) => (b.votes ?? 0) - (a.votes ?? 0)).map(item => (
-                                            <RoadmapItem key={item.title} item={item} />
-                                        ))}
-                                    </div>
-                                </>
+        <Stack spacing={4}>
+            {statusOrderedList.map(status => (
+                <Stack spacing={1} key={status}>
+                    <Typography level="h5">{camelToSentenceCase(status)}</Typography>
+                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8 }}>
+                        <ItemsWrapper isLoading={isLoading} error={error} loadingLabel={'Loading roadmap'} noItemsPlaceholder={'Empty at the moment'}>
+                            {orderBy(items?.filter(item => item.status === status) ?? [], (a, b) => (b.votes ?? 0) - (a.votes ?? 0)).map(item => (
+                                <RoadmapItem key={item.title} item={item} />
                             ))}
-                        </Stack>
-                    </Row>
-                ))}
-            </Stack>
-        </div>
+                        </ItemsWrapper>
+                    </div>
+                </Stack>
+            ))}
+        </Stack>
     );
 }
