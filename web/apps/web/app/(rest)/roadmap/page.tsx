564b563a15259cfb698fd5950e96a14e8debc1ec
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-02-28 23:38:01 +0100

    feat(Web): Roadmap loading issues on server-side with caching

--- web/apps/web/app/(rest)/roadmap/page.tsx
+++ web/apps/web/app/(rest)/roadmap/page.tsx
@@ -1,16 +1,9 @@
-'use client';
-
 import { Octokit } from 'octokit';
-import { Up } from '@signalco/ui-icons';
-import Stack from '@signalco/ui/dist/Stack';
-import { Button, ChildrenProps, Chip, Container, ItemsShowMore, Loadable, NavigatingButton, Sheet, Typography } from '@signalco/ui';
-import { camelToSentenceCase, orderBy } from '@signalco/js';
-import { useLoadAndError } from '@signalco/hooks';
-import PageCenterHeader from '../../../components/pages/PageCenterHeader';
+import Roadmap from './roadmap';
 
-type RoadmapItemStatus = 'triage' | 'planned' | 'inQueue' | 'inProgress' | 'completed';
+export type RoadmapItemStatus = 'triage' | 'planned' | 'inQueue' | 'inProgress' | 'completed';
 
-type RoadmapItem = {
+export type RoadmapItem = {
     title: string;
     status: RoadmapItemStatus;
     scope: string;
@@ -18,56 +11,28 @@ type RoadmapItem = {
     href?: string | undefined;
 }
 
-const statusOrderedList: RoadmapItemStatus[] = ['inProgress', 'inQueue', 'planned', 'triage', 'completed'];
-
-function VoteButton({ votes, readonly, size = 'md' }: { votes?: number | undefined, readonly?: boolean, size?: 'sm' | 'md' }) {
-    if (readonly) {
-        return (
-            <Sheet variant="soft" style={{ width: 42, aspectRatio: 1, textAlign: 'center', borderRadius: 'var(--joy-radius-sm)' }}>
-                <span style={{ lineHeight: '42px' }}>{votes ?? 'Vote'}</span>
-            </Sheet>
-        )
-    }
-
-    return (
-        <Button
-            size="sm"
-            style={{
-                paddingLeft: size === 'sm' ? 8 : undefined,
-                paddingRight: size === 'sm' ? 8 : undefined
-            }}>
-            <Stack alignItems="center" spacing={size === 'sm' ? 0 : 1} style={{ paddingTop: size === 'sm' ? 0 : 4 }}>
-                <Up />
-                <span style={{ paddingBottom: size === 'sm' ? 4 : 16 }}>{votes ?? 'Vote'}</span>
-            </Stack>
-        </Button>
-    );
-}
-
-function RoadmapItem({ item }: { item: RoadmapItem }) {
-    return (
-        <div key={item.title} style={{ height: '100%', display: 'flex', gap: 8, alignItems: 'center' }}>
-            <VoteButton votes={item.votes} readonly={true} size="sm" />
-            {item.href ? (
-                <NavigatingButton hideArrow href={item.href}>{item.title}</NavigatingButton>
-            ) : (
-                <Typography level="body2" noWrap textOverflow="ellipsis">{item.title}</Typography>
-            )}
-        </div>
-    );
-}
-
 async function getRoadmapItemsAsync() {
-    const octokit = new Octokit();
+    const octokit = new Octokit({ auth: process.env.GITHUB_PAT_TOKEN });
 
     const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
         owner: 'signalco-io',
         repo: 'signalco',
-        per_page: 50,
+        per_page: 100,
+        state: 'all',
+        sort: 'updated'
     });
 
     const items: RoadmapItem[] = [];
     for await (const { data: issues } of iterator) {
+        if (Array.isArray(issues) === false) {
+            return { error: 'Unexpected response from GitHub API' };
+        }
+
+        // Top 200 issues
+        if (issues.length >= 200) {
+            break;
+        }
+
         for (const issue of issues) {
             const labelNames = issue.labels.map(l => typeof l === 'string' ? l : l.name);
 
@@ -109,37 +74,13 @@ async function getRoadmapItemsAsync() {
             }
         }
     }
-    return items;
+    return {items};
 }
 
-export default function RoadmapPage() {
-    const { item: items, isLoading, error } = useLoadAndError(getRoadmapItemsAsync);
+export default async function RoadmapPage() {
+    const {items, error} = await getRoadmapItemsAsync();
 
     return (
-        <Container maxWidth="sm">
-            <Stack spacing={4}>
-                <PageCenterHeader header="Roadmap" subHeader="Help us by voting our roadmap." />
-                <Loadable isLoading={isLoading} error={error} loadingLabel={'Loading roadmap'}>
-                    <Stack spacing={2}>
-                        {statusOrderedList.map(status => (
-                            <Stack spacing={2} key={status}>
-                                <Typography level="h5">{camelToSentenceCase(status)}</Typography>
-                                <ItemsShowMore
-                                    truncate={5}
-                                    itemsWrapper={({ children }: ChildrenProps) => <Stack spacing={1}>{children}</Stack>}
-                                    isLoading={false}
-                                    error={undefined}
-                                    loadingLabel={'Loading roadmap'}
-                                    noItemsPlaceholder={'Empty at the moment'}>
-                                    {orderBy(items?.filter(item => item.status === status) ?? [], (a, b) => (b.votes ?? 0) - (a.votes ?? 0)).map(item => (
-                                        <RoadmapItem key={item.title} item={item} />
-                                    ))}
-                                </ItemsShowMore>
-                            </Stack>
-                        ))}
-                    </Stack>
-                </Loadable>
-            </Stack>
-        </Container>
+        <Roadmap items={items} error={error} />
     );
 }
