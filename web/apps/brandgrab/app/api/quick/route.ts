367e0dc2fad39c9a8bffd0d179e39b23818e8859
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-03-04 14:43:35 +0100

    feat(BrandGrab): Basic website info retrieval

---
+++ web/apps/brandgrab/app/api/quick/route.ts
@@ -0,0 +1,174 @@
+import {load} from 'cheerio';
+import { isAbsoluteUrl } from '@signalco/js';
+
+export type BrandResources = {
+    domain: string;
+    description: string;
+    title: string;
+    themeColor: string;
+    appleTouchIcon: string;
+    msTileImage: string;
+    msTileColor: string;
+    favicon: {
+        url: string;
+        base64: string;
+    };
+    icons: {
+        icon16: string;
+        icon16Base64: string;
+        icon32: string;
+        icon32Base64: string;
+        icon64: string;
+        icon64Base64: string;
+        icon128: string;
+        icon128Base64: string;
+        icon256: string;
+        icon256Base64: string;
+        icon512: string;
+        icon512Base64: string;
+    };
+    og: {
+        image: string;
+        imageBase64: string;
+        imageWidth: number | undefined;
+        imageHeight: number | undefined;
+        title: string;
+        description: string;
+        url: string;
+        siteName: string;
+        type: string;
+        locale: string;
+        localeAlternate: string;
+    };
+    twitter: {
+        card: string;
+    };
+}
+
+async function fetchDataUrl(url: string, domain: string) {
+    const response = await fetch(isAbsoluteUrl(url) ? url : `https://${domain}${url}`)
+    const blob = await response.blob();
+    const base64Data = Buffer.from(await blob.arrayBuffer()).toString('base64');
+    return `data:${response.headers.get('content-type')};base64,${base64Data}`;
+}
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const domain = searchParams.get('domain');
+    if (!domain) {
+        return Response.json({ error: 'No domain provided' }, { status: 400 });
+    }
+
+    const pageHtml = await fetch(`https://${domain}`);
+    const $ = load(await pageHtml.text());
+    const title = $('title')?.text() || '';
+    const description = $('meta[name="description"]')?.attr('content') || '';
+    const favicon = $('link[rel="icon"]')?.attr('href') || '';
+    const icon16 = $('link[rel="icon"][sizes="16x16"]')?.attr('href') || '';
+    const icon32 = $('link[rel="icon"][sizes="32x32"]')?.attr('href') || '';
+    const icon64 = $('link[rel="icon"][sizes="64x64"]')?.attr('href') || '';
+    const icon128 = $('link[rel="icon"][sizes="128x128"]')?.attr('href') || '';
+    const icon256 = $('link[rel="icon"][sizes="256x256"]')?.attr('href') || '';
+    const icon512 = $('link[rel="icon"][sizes="512x512"]')?.attr('href') || '';
+    const themeColor = $('meta[name="theme-color"]')?.attr('content') || '';
+    const appleTouchIcon = $('link[rel="apple-touch-icon"]')?.attr('href') || '';
+    const msTileImage = $('meta[name="msapplication-TileImage"]')?.attr('content') || '';
+    const msTileColor = $('meta[name="msapplication-TileColor"]')?.attr('content') || '';
+    const ogImage = $('meta[property="og:image"]')?.attr('content') || '';
+    const ogImageWidthString = $('meta[property="og:image:width"]')?.attr('content') || '';
+    const ogImageWidth: number | undefined = ogImageWidthString ? parseInt(ogImageWidthString) : undefined;
+    const ogImageHeightString = $('meta[property="og:image:height"]')?.attr('content') || '';
+    const ogImageHeight: number | undefined = ogImageHeightString ? parseInt(ogImageHeightString) : undefined;
+    const ogTitle = $('meta[property="og:title"]')?.attr('content') || '';
+    const ogDescription = $('meta[property="og:description"]')?.attr('content') || '';
+    const ogUrl = $('meta[property="og:url"]')?.attr('content') || '';
+    const ogSiteName = $('meta[property="og:site_name"]')?.attr('content') || '';
+    const ogType = $('meta[property="og:type"]')?.attr('content') || '';
+    const ogLocale = $('meta[property="og:locale"]')?.attr('content') || '';
+    const ogLocaleAlternate = $('meta[property="og:locale:alternate"]')?.attr('content') || '';
+    const twitterCard = $('meta[name="twitter:card"]')?.attr('content') || '';
+
+    let favIcoBase64 = '';
+    if (favicon.length > 0) {
+        favIcoBase64 = await fetchDataUrl(favicon, domain) ?? '';
+    }
+
+    let ogImageBase64 = '';
+    if (ogImage.length > 0) {
+        ogImageBase64 = await fetchDataUrl(ogImage, domain) ?? '';
+    }
+
+    let icon16Base64 = '';
+    if (icon16.length > 0) {
+        icon16Base64 = await fetchDataUrl(icon16, domain) ?? '';
+    }
+
+    let icon32Base64 = '';
+    if (icon32.length > 0) {
+        icon32Base64 = await fetchDataUrl(icon32, domain) ?? '';
+    }
+
+    let icon64Base64 = '';
+    if (icon64.length > 0) {
+        icon64Base64 = await fetchDataUrl(icon64, domain) ?? '';
+    }
+
+    let icon128Base64 = '';
+    if (icon128.length > 0) {
+        icon128Base64 = await fetchDataUrl(icon128, domain) ?? '';
+    }
+
+    let icon256Base64 = '';
+    if (icon256.length > 0) {
+        icon256Base64 = await fetchDataUrl(icon256, domain) ?? '';
+    }
+
+    let icon512Base64 = '';
+    if (icon512.length > 0) {
+        icon512Base64 = await fetchDataUrl(icon512, domain) ?? '';
+    }
+
+    return Response.json({
+        domain,
+        title,
+        description,
+        themeColor,
+        appleTouchIcon,
+        msTileImage,
+        msTileColor,
+        favicon: {
+            url: favicon,
+            base64: favIcoBase64
+        },
+        icons: {
+            icon16,
+            icon16Base64,
+            icon32,
+            icon32Base64,
+            icon64,
+            icon64Base64,
+            icon128,
+            icon128Base64,
+            icon256,
+            icon256Base64,
+            icon512,
+            icon512Base64
+        },
+        og: {
+            image: ogImage,
+            imageBase64: ogImageBase64,
+            imageWidth: ogImageWidth,
+            imageHeight: ogImageHeight,
+            title: ogTitle,
+            description: ogDescription,
+            url: ogUrl,
+            siteName: ogSiteName,
+            type: ogType,
+            locale: ogLocale,
+            localeAlternate: ogLocaleAlternate
+        },
+        twitter: {
+            card: twitterCard
+        },
+    } satisfies BrandResources);
+}
--- web/apps/brandgrab/components/views/LandingView.tsx
+++ web/apps/brandgrab/components/views/LandingView.tsx
@@ -1,14 +1,169 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Stack from '@signalco/ui/dist/Stack';
-import { StepContent } from '../pages/StepContent';
-import { NewsletterSection } from '../pages/NewsletterSection';
+import { Button, Card, CardContent, CardCover, ChildrenProps, Container, Row, TextField, Typography } from '@signalco/ui';
+import { useSearchParam } from '@signalco/hooks';
+import PageCenterHeader from '../pages/PageCenterHeader';
+import { BrandResources } from '../../app/api/quick/route';
+
+function BrandGrabInput({ value, onChange }: { value: string | undefined, onChange: (value: string) => void }) {
+    const [domain, setDomain] = useState(value || '');
+
+    return (
+        <Stack>
+            <Row spacing={1}>
+                <TextField placeholder="Enter domain name" value={domain} onChange={(e) => setDomain(e.target.value)} />
+                <Button onClick={() => onChange(domain)}>Go</Button>
+            </Row>
+        </Stack>
+    )
+}
+
+function OgPreview({ og }: { og: BrandResources['og'] }) {
+    return (
+        <Card style={{ width: 400, minHeight: 209 }}>
+            <CardCover>
+                <Image
+                    src={og.imageBase64}
+                    alt="og:image"
+                    width={400}
+                    height={209} />
+            </CardCover>
+            <CardCover
+                sx={{
+                    background:
+                        'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 100px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 100px)',
+                }}
+            />
+            <CardContent style={{ justifyContent: 'flex-end' }}>
+                <Stack>
+                    <Typography textColor="#fff" level="h5" style={{
+                        marginTop: 8
+                    }}>{og.title}</Typography>
+                    <Typography textColor="neutral.300">{og.description}</Typography>
+                </Stack>
+            </CardContent>
+        </Card>
+    )
+}
+
+function IconPreview({ favicon, icons }: { favicon: BrandResources['favicon'], icons: BrandResources['icons'] }) {
+    return (
+        <div style={{
+            width: 144 + 16 * 2 + 42 + 16 * 2,
+            height: 144 + 16 * 2,
+            display: 'grid',
+            gridTemplateColumns: '176px 74px',
+            gridTemplateRows: '74px 48px',
+            gap: 16
+        }}>
+            <Image
+                style={{
+                    width: 144+16*2,
+                    height: 144+16*2,
+                    gridRowEnd: 'span 2',
+                    padding: 16,
+                    border: '1px solid var(--joy-palette-divider)',
+                    borderRadius: 8
+                }}
+                src={icons.icon256Base64 || icons.icon128Base64 || icons.icon64Base64 || icons.icon32Base64 || icons.icon16Base64 || favicon.base64}
+                alt="favicon"
+                width={144}
+                height={144} />
+            <Image
+                style={{
+                    width: 42+16*2,
+                    height: 42+16*2,
+                    padding: 16,
+                    border: '1px solid var(--joy-palette-divider)',
+                    borderRadius: 8
+                }}
+                src={icons.icon64Base64 || icons.icon32Base64 || icons.icon16Base64 || favicon.base64}
+                alt="favicon"
+                width={42}
+                height={42} />
+            <Image
+                style={{
+                    width: 16*3,
+                    height: 16*3,
+                    padding: 16,
+                    border: '1px solid var(--joy-palette-divider)',
+                    borderRadius: 8
+                }}
+                src={icons.icon16Base64 || favicon.base64}
+                alt="favicon"
+                width={16}
+                height={16} />
+        </div>
+    )
+}
+
+function TextInfo({ title, children }: { title: string } & ChildrenProps) {
+    return (
+        <Stack>
+            <Typography level="body3">{title}</Typography>
+            <div>{children || '-'}</div>
+        </Stack>
+    )
+}
+
+function BrandView({ resources }: { resources: BrandResources | undefined }) {
+    if (!resources) return null;
+
+    return (
+        <Stack spacing={1}>
+            <TextInfo title="Domain">
+                {resources.domain}
+            </TextInfo>
+            <TextInfo title="Title">
+                {resources.title}
+            </TextInfo>
+            <TextInfo title="Description">
+                {resources.description}
+            </TextInfo>
+            <TextInfo title="Favicon & Icons">
+                <IconPreview favicon={resources.favicon} icons={resources.icons} />
+            </TextInfo>
+            <TextInfo title="Open Graph (og)">
+                <OgPreview og={resources.og} />
+            </TextInfo>
+        </Stack>
+    );
+}
+
+async function quickLookup(domain: string): Promise<BrandResources> {
+    return await fetch('/api/quick?domain=' + domain).then(res => res.json()).then(res => res as BrandResources);
+}
+
+function stripDomain(domain: string) {
+    return domain.replace(/https?:\/\//, '');
+}
 
 export default function LandingPageView() {
+    const [domain, setDomain] = useSearchParam('domain');
+    const [domainResources, setDomainResources] = useState<BrandResources | undefined>(undefined);
+
+    const handleDomainChange = (domain: string) => {
+        setDomain(stripDomain(domain));
+    }
+
+    useEffect(() => {
+        if (domain) {
+            quickLookup(domain).then(setDomainResources);
+        }
+    }, [domain])
+
     return (
-        <Stack style={{ overflowX: 'hidden' }}>
-            <StepContent header="BrandGrab">
-            </StepContent>
-            <NewsletterSection />
+        <Stack style={{ overflowX: 'hidden', paddingBottom: 16 }}>
+            <PageCenterHeader header={'BrandGrab'} />
+            <Container>
+                <Stack spacing={2}>
+                    <BrandGrabInput value={domain} onChange={handleDomainChange} />
+                    <BrandView resources={domainResources} />
+                </Stack>
+            </Container>
         </Stack>
     );
 }
