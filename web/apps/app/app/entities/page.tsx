1fcc9448d86adae735fad336c7997328111b7ab4
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-03-07 00:50:17 +0100

    feat(App): Added bulk delete to entities list page

--- web/apps/app/app/entities/page.tsx
+++ web/apps/app/app/entities/page.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { Add, LayoutGrid, LayoutList } from '@signalco/ui-icons';
+import { useQueryClient } from '@tanstack/react-query';
+import { Add, Check, LayoutGrid, LayoutList } from '@signalco/ui-icons';
 import { Stack, Loadable, Row, Avatar, Button, IconButton, Typography, Box, Grid, SelectItems, Picker, MuiStack } from '@signalco/ui';
 import { KnownPages } from '../../src/knownPages';
 import useUserSetting from '../../src/hooks/useUserSetting';
 import useLocale from '../../src/hooks/useLocale';
 import useAllEntities from '../../src/hooks/signalco/entity/useAllEntities';
 import IEntityDetails from '../../src/entity/IEntityDetails';
-import { entityUpsertAsync } from '../../src/entity/EntityRepository';
+import { entityDeleteAsync, entityUpsertAsync } from '../../src/entity/EntityRepository';
 import SearchInput from '../../components/shared/inputs/SearchInput';
 import { EntityIconByType } from '../../components/shared/entity/EntityIcon';
+import ConfirmDeleteButton from '../../components/shared/dialog/ConfirmDeleteButton';
 import ConfigurationDialog from '../../components/shared/dialog/ConfigurationDialog';
 import EntityCard from '../../components/entity/EntityCard';
 
@@ -31,7 +33,7 @@ const createEntityType = [
     { value: 5, label: 'Channel' }
 ];
 
-function EntityCreate() {
+function EntityCreateForm() {
     const router = useRouter();
     const { t } = useLocale('App', 'Entities', 'NewEntityDialog');
     const { t: tType } = useLocale('Global', 'Entity', 'Types');
@@ -61,6 +63,27 @@ export default function Entities() {
     const [entityListViewType, setEntityListViewType] = useUserSetting<string>('entityListViewType', 'table');
     const [filteredItems, setFilteredItems] = useState<IEntityDetails[] | undefined>(entities.data);
 
+    const [isSelecting, setIsSelecting] = useState(false);
+    const [selected, setSelected] = useState<IEntityDetails[]>([]);
+    const handleToggleSelection = () => {
+        setIsSelecting(!isSelecting);
+    };
+    const handleEntitySelection = useCallback((entity: IEntityDetails) => {
+        if (!selected.find(e => e.id === entity.id)) {
+            setSelected([...selected, entity]);
+        } else {
+            setSelected(selected.filter(e => e.id !== entity.id));
+        }
+    }, [selected]);
+
+    const queryClient = useQueryClient();
+    const handleConfirmDeleteSelected = async () => {
+        await Promise.all(selected.map(e => entityDeleteAsync(e.id)));
+        queryClient.invalidateQueries(['entities']);
+        queryClient.invalidateQueries(['entities', parseInt(selectedType, 10)]);
+        setSelected([]);
+    };
+
     const typedItems = useMemo(() => filteredItems?.filter(e => {
         return e.type.toString() === selectedType;
     }), [filteredItems, selectedType]);
@@ -77,12 +100,17 @@ export default function Entities() {
             <Grid container spacing={1}>
                 {typedItems?.map(entity => (
                     <Grid key={entity.id} {...columns}>
-                        <EntityCard entity={entity} spread={entityListViewType === 'table'} />
+                        <EntityCard
+                            entity={entity}
+                            spread={entityListViewType === 'table'}
+                            selectable={isSelecting}
+                            selected={!!selected.find(e => e.id === entity.id)}
+                            onSelection={() => handleEntitySelection(entity)} />
                     </Grid>
                 ))}
             </Grid>
         </Box>
-    ), [columns, entityListViewType, typedItems]);
+    ), [columns, entityListViewType, handleEntitySelection, isSelecting, selected, typedItems]);
 
     const [isAddEntityOpen, setIsAddEntityOpen] = useState(false);
     const handleAddEntity = () => setIsAddEntityOpen(true);
@@ -90,32 +118,44 @@ export default function Entities() {
     return (
         <>
             <MuiStack spacing={{ xs: 2, sm: 4 }} sx={{ pt: { xs: 0, sm: 2 } }}>
-                <Row justifyContent="space-between" style={{ paddingLeft: 16, paddingRight: 16 }}>
-                    <SelectItems
-                        minWidth={220}
-                        value={selectedType ? [selectedType] : []}
-                        onChange={(v) => setSelectedType(v[0])}
-                        items={entityTypes.map(t => {
-                            const Icon = EntityIconByType(parseInt(t.value));
-                            return ({
-                                value: t.value, label: t.label, content: (
-                                    <Row spacing={1}>
-                                        <Avatar><Icon /></Avatar>
-                                        <Typography>{t.label}</Typography>
-                                    </Row>
-                                )
-                            });
-                        })}
-                        heading />
-                    <MuiStack direction="row" alignItems="center" spacing={1} sx={{ flexGrow: { xs: 1, sm: 0 } }} justifyContent="end">
-                        <SearchInput items={entities.data} onFilteredItems={setFilteredItems} />
-                        <Picker value={entityListViewType} onChange={(_, value) => setEntityListViewType(value)} options={[
-                            { value: 'table', label: <LayoutList /> },
-                            { value: 'cards', label: <LayoutGrid /> }
-                        ]} />
-                        <IconButton size="lg" onClick={handleAddEntity}><Add /></IconButton>
-                    </MuiStack>
-                </Row>
+                <Stack spacing={1}>
+                    <Row justifyContent="space-between" style={{ paddingLeft: 16, paddingRight: 16 }}>
+                        <SelectItems
+                            minWidth={220}
+                            value={selectedType ? [selectedType] : []}
+                            onChange={(v) => setSelectedType(v[0])}
+                            items={entityTypes.map(t => {
+                                const Icon = EntityIconByType(parseInt(t.value));
+                                return ({
+                                    value: t.value, label: t.label, content: (
+                                        <Row spacing={1}>
+                                            <Avatar><Icon /></Avatar>
+                                            <Typography>{t.label}</Typography>
+                                        </Row>
+                                    )
+                                });
+                            })}
+                            heading />
+                        <MuiStack direction="row" alignItems="center" spacing={1} sx={{ flexGrow: { xs: 1, sm: 0 } }} justifyContent="end">
+                            <SearchInput items={entities.data} onFilteredItems={setFilteredItems} />
+                            <Picker value={entityListViewType} onChange={(_, value) => setEntityListViewType(value)} options={[
+                                { value: 'table', label: <LayoutList /> },
+                                { value: 'cards', label: <LayoutGrid /> }
+                            ]} />
+                            <IconButton size="lg" onClick={handleAddEntity}><Add /></IconButton>
+                            <IconButton size="lg" onClick={handleToggleSelection}><Check /></IconButton>
+                        </MuiStack>
+                    </Row>
+                    {isSelecting && (
+                        <Row>
+                            <ConfirmDeleteButton
+                                expectedConfirmText="confirm"
+                                buttonLabel="Delete selected"
+                                header={`Delete ${selected.length} entities`}
+                                onConfirm={handleConfirmDeleteSelected} />
+                        </Row>
+                    )}
+                </Stack>
                 <Stack>
                     <Loadable isLoading={entities.isLoading} loadingLabel="Loading entities" error={entities.error}>
                         {results}
@@ -123,7 +163,7 @@ export default function Entities() {
                 </Stack>
             </MuiStack>
             <ConfigurationDialog isOpen={isAddEntityOpen} header={t('NewEntityDialogTitle')} onClose={() => setIsAddEntityOpen(false)}>
-                <EntityCreate />
+                <EntityCreateForm />
             </ConfigurationDialog>
         </>
     );
