29b909717c11243427dd79c28563b007a016eae0
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-03-21 00:13:56 +0100

    feat(cloud): Moved migration script to separate function
    #2281

---
+++ cloud/src/Signalco.Func.Internal.Migration/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Signal.Api.Common;
+using Signal.Core;
+using Signal.Infrastructure.AzureStorage.Tables;
+using Signal.Infrastructure.Secrets;
+using Signalco.Func.Internal.Migration;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Signalco.Func.Internal.Migration;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services
+            .AddCore()
+            .AddApi()
+            .AddSecrets()
+            .AddAzureStorage();
+    }
+}
