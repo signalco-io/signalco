7391ce420a951f43efe818fd808e28d6f21de3b2
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-03-23 13:27:20 +0100

    fix(web): Fixed e2e tests importing other tests

--- web/apps/web/e2e/station.spec.ts
+++ web/apps/web/e2e/station.spec.ts
@@ -1,4 +1,4 @@
 import { test } from '@playwright/test'
-import { assertAccessibility } from './landing.spec';
+import { assertAccessibility } from './shared';
 
 test('channels should be accessible', ({ page }) => assertAccessibility(page, '/station'));
