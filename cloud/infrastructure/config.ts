033671fe0f4693635553d251bea4113d290a5d99
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-03-27 14:51:54 +0200

    feat(infrastructure): Increased Checkly checks interval to reduce usage

---
+++ cloud/infrastructure/config.ts
@@ -0,0 +1,6 @@
+import { CheckFrequency } from './Checkly/apiStatusCheck';
+
+export const ConfChannelApiCheckInterval: CheckFrequency = 120;
+export const ConfCloudApiCheckInterval: CheckFrequency = 60;
+export const ConfInternalApiCheckInterval: CheckFrequency = 180;
+export const ConfPublicSignalRCheckInterval: CheckFrequency = 180;
