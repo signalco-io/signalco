b8fd93f94afae91be3f36159fa2b415d8e0855c6
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-04-02 02:36:43 +0200

    feat(brandgrab): Migrated from UrlBox to Signalco RemoteBrowser

--- web/apps/brandgrab/app/api/screenshot/route.ts
+++ web/apps/brandgrab/app/api/screenshot/route.ts
@@ -2,7 +2,7 @@ import sharp from 'sharp';
 import { NextResponse } from 'next/server';
 import getPixels from 'get-pixels';
 import { extractColors } from 'extract-colors';
-import { HmacSHA1 } from 'crypto-js';
+import { isDeveloper } from '../../../src/services/EnvProvider';
 
 function getPixelsAsync(url: string) {
     return new Promise<{data: Uint8ClampedArray, width?: number | undefined, height?: number | undefined}>((resolve, reject) => {
@@ -29,9 +29,8 @@ export async function GET(request: Request) {
         return NextResponse.json({ error: 'No domain provided' }, { status: 400 });
     }
 
-    const query = `url=${domain}&full_page=true&width=1280&hide_cookie_banners=true`;
-    const token = HmacSHA1(query, process.env.URLBOX_SECRET ?? '');
-    const response = await fetch(`https://api.urlbox.io/v1/${process.env.URLBOX_KEY}/${token}/webp?${query}`);
+    const query = `url=${domain}&fullPage=true&width=1280&scrollThrough=true`;
+    const response = await fetch(`https://browser.api.signalco.${isDeveloper ? 'dev' : 'io'}/api/screenshot?${query}`);
     const data = Buffer.from(await response.arrayBuffer());
     const dataUrlDataWebp = `data:image/webp;base64,${data.toString('base64')}`;
 
