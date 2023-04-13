0665c5c865dffcc6cddd7067522ff8fab19f766c
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-04-13 18:53:38 +0200

    chore(cloud): Fixed health functions implementations

--- cloud/src/Signalco.Channel.GitHubApp/Functions/HealthFunctions.cs
+++ cloud/src/Signalco.Channel.GitHubApp/Functions/HealthFunctions.cs
@@ -5,17 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Signal.Core.Secrets;
 using Signalco.Common.Channel;
 
 namespace Signalco.Channel.PhilipsHue.Functions;
 
 public class HealthFunctions : HealthStatusFunctionsBase
 {
-    public HealthFunctions(ISecretsManager secretsManager) : base(secretsManager)
-    {
-    }
-
     [FunctionName("Status")]
     [OpenApiOperation(operationId: nameof(HealthFunctions), tags: new[] {"Health"})]
     [OpenApiResponseWithoutBody(HttpStatusCode.OK, Description = "API is running.")]
