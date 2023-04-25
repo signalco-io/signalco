4b902b423d8da3cdd12ef5ae35034554fb6e5f21
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-04-25 21:53:51 +0200

    fix(infrastructure): Precise SAS timestamps

--- cloud/infrastructure/src/Azure/assignFunctionCodeAsync.ts
+++ cloud/infrastructure/src/Azure/assignFunctionCodeAsync.ts
@@ -1,4 +1,5 @@
-import { asset, type Input, type Resource } from '@pulumi/pulumi';
+import { type Input, type Resource } from '@pulumi/pulumi';
+import { FileArchive } from '@pulumi/pulumi/asset';
 import { type ResourceGroup } from '@pulumi/azure-native/resources';
 import { Blob, type BlobContainer, type StorageAccount } from '@pulumi/azure-native/storage';
 import { signedBlobReadUrl } from './signedBlobReadUrl';
@@ -10,18 +11,15 @@ export async function assignFunctionCodeAsync(
     namePrefix: string,
     codePath: string,
     dependsOn?: Input<Resource> | Input<Input<Resource>[]> | undefined) {
-    const archive = new asset.FileArchive(codePath);
-
     // Upload Azure Function's code as a zip archive to the storage account.
     const codeBlob = new Blob(`zip-${namePrefix}`, {
         blobName: `zip-${namePrefix}`,
         resourceGroupName: resourceGroup.name,
         accountName: storageAccount.name,
         containerName: zipsContainer.name,
-        source: archive,
+        source: new FileArchive(codePath),
     }, {
         dependsOn,
-        // parent: app
     });
 
     const codeBlobUrl = signedBlobReadUrl(codeBlob, zipsContainer, storageAccount, resourceGroup);
