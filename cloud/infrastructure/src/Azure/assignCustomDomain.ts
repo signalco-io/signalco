e9268192e7262aee7c28124b37ba462355567d3a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-04-28 15:23:32 +0200

    feat(infrastructure): Using StackReference to handle custom domain assignment instead of local commands

--- cloud/infrastructure/src/Azure/assignCustomDomain.ts
+++ cloud/infrastructure/src/Azure/assignCustomDomain.ts
@@ -1,10 +1,16 @@
-import { Config, interpolate, log } from '@pulumi/pulumi';
+import { Config, interpolate, type StackReference, Output } from '@pulumi/pulumi';
 import { WebApp, AppServicePlan, WebAppHostNameBinding, Certificate, HostNameType, SslState, CustomHostNameDnsRecordType } from '@pulumi/azure-native/web';
 import { ResourceGroup } from '@pulumi/azure-native/resources';
-import { local } from '@pulumi/command';
 import dnsRecord from '../CloudFlare/dnsRecord';
 
-export function assignCustomDomain (resourceGroup: ResourceGroup, webApp: WebApp, servicePlan: AppServicePlan, namePrefix: string, subDomainName: string, protect: boolean) {
+export function assignCustomDomain (
+    resourceGroup: ResourceGroup, 
+    webApp: WebApp, 
+    servicePlan: AppServicePlan, 
+    namePrefix: string, 
+    subDomainName: string, 
+    currentStack: StackReference,
+    protect: boolean) {
     const config = new Config();
     const fullDomainName = `${subDomainName}.${config.require('domain')}`;
 
@@ -18,24 +24,26 @@ export function assignCustomDomain (resourceGroup: ResourceGroup, webApp: WebApp
     const cname = dnsRecord(
         `func-dns-cname-${namePrefix}`,
         fullDomainName,
-        interpolate`${webApp.name}.azurewebsites.net`,
+        webApp.defaultHostName,
         'CNAME',
         protect,
         webApp,
     );
 
-    // Until Pulumi comes up with circular dependency solution
-    // we are creating hostname binding via Azure CLI because
-    // it's required to create certificate, only then the
-    // hostname binding with SSL cert can be configured
-    const assignHostName = new local.Command(`func-hostname-assign-${namePrefix}`, {
-        create: interpolate`az webapp config hostname add --webapp-name ${webApp.name} --resource-group ${resourceGroup.name} --hostname ${fullDomainName}`,
-    }, {
-        dependsOn: [txtVerifyRecord, cname],
-        // parent: webApp
-    });
+    const certsOutput = currentStack.getOutput('certs') as Output<{ fullDomainName: string, thumbprint: string }[]>;
+    const certOutput = certsOutput.apply(certs => certs?.find(c => c.fullDomainName === fullDomainName));
 
-    assignHostName.stderr.apply(err => err && log.warn('Assign hostname failed with: ' + err));
+    const binding = new WebAppHostNameBinding(`func-hostnamebind-${namePrefix}`, {
+        name: webApp.name,
+        resourceGroupName: resourceGroup.name,
+        hostName: fullDomainName,
+        customHostNameDnsRecordType: CustomHostNameDnsRecordType.CName,
+        hostNameType: HostNameType.Verified,
+        sslState: certOutput.apply(c => c?.thumbprint ? SslState.SniEnabled : SslState.Disabled),
+        thumbprint: certOutput.apply(c => c?.thumbprint ?? ''),
+    }, {
+        protect,
+    });
 
     const cert = new Certificate(`func-cert-${namePrefix}`, {
         resourceGroupName: resourceGroup.name,
@@ -43,31 +51,6 @@ export function assignCustomDomain (resourceGroup: ResourceGroup, webApp: WebApp
         serverFarmId: servicePlan.id,
     }, {
         protect,
-        dependsOn: [assignHostName],
-        // parent: webApp
-    });
-
-    const deleteHostName = new local.Command(`func-hostname-remove-${namePrefix}`, {
-        create: interpolate`az webapp config hostname delete --webapp-name ${webApp.name} --resource-group ${resourceGroup.name} --hostname ${fullDomainName}`,
-    }, {
-        dependsOn: [cert],
-        // parent: webApp
-    });
-
-    deleteHostName.stderr.apply(err => err && log.warn('Delete hostname failed with: ' + err));
-
-    const binding = new WebAppHostNameBinding(`func-hostnamebind-${namePrefix}`, {
-        name: webApp.name,
-        resourceGroupName: resourceGroup.name,
-        hostName: fullDomainName,
-        hostNameType: HostNameType.Verified,
-        sslState: SslState.SniEnabled,
-        customHostNameDnsRecordType: CustomHostNameDnsRecordType.CName,
-        thumbprint: cert.thumbprint,
-    }, {
-        dependsOn: [deleteHostName],
-        protect,
-        // parent: webApp
     });
 
     return {
