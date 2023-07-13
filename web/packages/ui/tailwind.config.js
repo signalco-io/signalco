06ad177bde7048abcfb72d3e1ff6c7c40e7362a4
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-07-13 20:45:36 +0200

    fix(ui): Prefix class names to avoid collisions with apps

--- web/packages/ui/tailwind.config.js
+++ web/packages/ui/tailwind.config.js
@@ -1,9 +1,7 @@
-const { baseConfig } = require("@signalco/tailwindcss-config-signalco");
+const { baseConfig } = require('@signalco/tailwindcss-config-signalco');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [baseConfig],
-    content: [
-        './src/**/*.{ts,tsx}'
-    ]
+    prefix: 'ui-',
+    presets: [baseConfig]
 }
