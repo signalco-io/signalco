56deb31cb911a3388dab549cfc080ab63442b01d
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-07-29 21:17:51 +0200

    feat(station): Update using configured channel (next or production)

--- station/Signal.Beacon.Application/Lifetime/LinuxUpdateService.cs
+++ station/Signal.Beacon.Application/Lifetime/LinuxUpdateService.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Signal.Beacon.Application.Signal.Station;
+using Signal.Beacon.Core.Entity;
 using Signal.Beacon.Core.Shell;
 
 namespace Signal.Beacon.Application.Lifetime;
@@ -11,13 +13,19 @@ public class LinuxUpdateService : IUpdateService
 {
     private const string FilePathExecute = "./rpi-update.sh";
 
+    private readonly IStationStateService stateService;
+    private readonly IEntitiesDao entitiesDao;
     private readonly IShellService shell;
     private readonly ILogger<LinuxUpdateService> logger;
 
     public LinuxUpdateService(
+        IStationStateService stateService,
+        IEntitiesDao entitiesDao,
         IShellService shell,
         ILogger<LinuxUpdateService> logger)
     {
+        this.stateService = stateService ?? throw new ArgumentNullException(nameof(stateService));
+        this.entitiesDao = entitiesDao ?? throw new ArgumentNullException(nameof(entitiesDao));
         this.shell = shell ?? throw new ArgumentNullException(nameof(shell));
         this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
@@ -54,8 +62,17 @@ public class LinuxUpdateService : IUpdateService
         this.logger.LogDebug("Setting permission for update script...");
         await this.shell.ExecuteShellCommandAsync($"chmod +x {FilePathExecute}", cancellationToken);
 
+        // Determine current channel
+        var releaseChannelArg = string.Empty;
+        var state = await this.stateService.GetAsync(cancellationToken);
+        var stationReleaseChannel = await this.entitiesDao.ContactValueSerializedAsync(
+            new ContactPointer(state.Id, "signalco", "stationReleaseChannel"),
+            cancellationToken);
+        if (stationReleaseChannel == "next")
+            releaseChannelArg = "--next";
+
         this.logger.LogInformation("Starting station update...");
         var fileInfo = new FileInfo(FilePathExecute);
-        await this.shell.ExecuteShellCommandAsync($"sudo {fileInfo.FullName}", cancellationToken);
+        await this.shell.ExecuteShellCommandAsync($"sudo {fileInfo.FullName} {releaseChannelArg}", cancellationToken);
     }
 }
