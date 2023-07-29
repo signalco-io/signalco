f7387b441ae4b2331bef9b34e7fc1a4fb7720ad0
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-07-29 21:58:53 +0200

    feat(web): Added security page
    Closes #223

--- web/packages/ui/src/Avatar/Avatar.tsx
+++ web/packages/ui/src/Avatar/Avatar.tsx
@@ -1,11 +1,16 @@
+import { cx } from 'classix';
 import { PropsWithChildren } from 'react';
 
 export type AvatarProps = PropsWithChildren<{
     size?: 'sm' | 'md' | 'lg'; // TODO: Implement
     src?: string; // TODO: Implement
     alt?: string; // TODO: Implement
+    className?: string;
 }>;
 
-export function Avatar({ children }: AvatarProps) {
-    return <div className="flex h-10 w-10 items-center justify-center rounded-full bg-muted">{children}</div>;
+export function Avatar({ children, className }: AvatarProps) {
+    return <div className={cx(
+        "flex h-10 min-w-[40px] max-w-[40px] items-center justify-center rounded-full bg-muted",
+        className
+    )}>{children}</div>;
 }
