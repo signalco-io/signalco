3b1e34a4d41f09dadeb54d1018381a5bb7b1d149
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-09-21 10:19:02 +0200

    feat(web): Split footer into smaller components

---
+++ web/apps/web/components/pages/Footer/FooterContainer.tsx
@@ -0,0 +1,14 @@
+import { type PropsWithChildren } from 'react';
+import { Container } from '@signalco/ui/dist/Container';
+
+export function FooterContainer({ children }: PropsWithChildren) {
+    return (
+        <footer className="self-stretch">
+            <Container maxWidth="lg">
+                <div className="pb-8 pt-16">
+                    {children}
+                </div>
+            </Container>
+        </footer>
+    );
+}
