59aaa8dc29efdad04438e33d24656b67f13e54bb
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-09-26 22:27:07 +0200

    feat: Added Signalco CLI

---
+++ src/Signalco.CLI/Signalco.CLI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+
+var rootCommand = new RootCommand("Signalco CLI tool.");
+
+var authLoginCommand = new Command("login", "Login with your account.");
+authLoginCommand.SetHandler(HandleAuthLoginAsync);
+authLoginCommand.AddValidator(Validate);
+var authLogoutCommand = new Command("logout", "Logout with currently logged in account.");
+var authWhoIsCommand = new Command("who", "Display currently logged in account.");
+
+rootCommand.AddCommand(authLoginCommand);
+rootCommand.AddCommand(authLogoutCommand);
+rootCommand.AddCommand(authWhoIsCommand);
+
+return await rootCommand.InvokeAsync(args);
+
+async Task Validate(CommandResult symbolResult)
+{
+    symbolResult.
+}
+
+async Task HandleAuthLoginAsync(InvocationContext context)
+{
+    var cancellationToken = context.GetCancellationToken();
+    
+}
