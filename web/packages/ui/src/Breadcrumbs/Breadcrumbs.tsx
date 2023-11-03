87706b324a934e5ef9a9e8c38626c3b3f7506e52
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-11-03 10:53:53 +0100

    feat(ui): Breadcrumbs now accept ReactNode as breadcrumb item label

--- web/packages/ui/src/Breadcrumbs/Breadcrumbs.tsx
+++ web/packages/ui/src/Breadcrumbs/Breadcrumbs.tsx
@@ -1,11 +1,11 @@
-import { Fragment } from 'react';
+import { Fragment, type ReactNode } from 'react';
 import { Typography } from '../Typography';
 import { Row } from '../Row';
 import { BreadcrumbsItem } from './BreadcrumbsItem';
 
 export type BreadcrumbItem = {
     href?: string;
-    label: string | undefined;
+    label: ReactNode | string | undefined;
 };
 
 export type BreadcrumbsProps = {
@@ -19,7 +19,7 @@ export function Breadcrumbs({ items }: BreadcrumbsProps) {
     return (
         <Row spacing={1}>
             {items.map((item, index) => (
-                <Fragment key={item.label}>
+                <Fragment key={item.href}>
                     <BreadcrumbsItem href={item.href} label={item.label} />
                     {index < items.length - 1 && <Typography fontSize={'1.3em'}>{'\u203a'}</Typography>}
                 </Fragment>
