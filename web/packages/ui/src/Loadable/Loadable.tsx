fc53737b27734937aa271e374c2905116329463a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-11-03 10:53:14 +0100

    feat(ui): Added Loadable placeholders which were removed when migrating to TailwindCSS

--- web/packages/ui/src/Loadable/Loadable.tsx
+++ web/packages/ui/src/Loadable/Loadable.tsx
@@ -1,6 +1,7 @@
-import { type PropsWithChildren, useMemo, ReactNode } from 'react';
+import { type PropsWithChildren, useMemo, ReactNode, CSSProperties } from 'react';
 import { cx } from 'classix';
 import { Warning } from '@signalco/ui-icons';
+import { Skeleton } from '../Skeleton';
 import { Alert } from '../Alert';
 
 export type LoadableLoadingErrorProps = {
@@ -20,21 +21,19 @@ export type LoadableProps = PropsWithChildren<LoadableLoadingErrorProps & Loadab
     className?: string;
 }>;
 
-export function Loadable(props: LoadableProps) {
-    const { isLoading, loadingLabel, placeholder, error, children, contentVisible, className } = props;
-
+export function Loadable({
+    isLoading,
+    loadingLabel,
+    placeholder,
+    error,
+    children,
+    contentVisible,
+    className,
+    width,
+    height
+}: LoadableProps) {
     const indicator = useMemo(() => {
-        if (typeof placeholder === 'object') {
-            return placeholder;
-        }
-
-        switch (placeholder) {
-        case 'skeletonText':
-            // return <Skeleton variant="text" width={width ?? 120} />;
-        case 'skeletonRect':
-            // return <Skeleton variant="rectangular" width={width ?? 120} height={height ?? 32} />;
-        case 'circular':
-        default:
+        if (placeholder == null || placeholder === 'circular') {
             return (
                 <div className={cx('uitw-flex uitw-flex-col uitw-items-center uitw-p-2', className)}>
                     <svg className="uitw-h-8 uitw-w-8 uitw-animate-spin uitw-text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-label={loadingLabel}>
@@ -44,7 +43,26 @@ export function Loadable(props: LoadableProps) {
                 </div>
             );
         }
-    }, [className, loadingLabel, placeholder]);
+
+        if (placeholder === 'skeletonText') {
+            return (
+                <Skeleton
+                    className={cx('uitw-h-5 uitw-w-[--width]', className)}
+                    style={{ '--width': `${width ?? 120}px`} as CSSProperties}
+                />
+            );
+        }
+        if (placeholder === 'skeletonRect') {
+            return (
+                <Skeleton
+                    className={cx('uitw-h-[--height] uitw-w-[--width]', className)}
+                    style={{ '--width': `${width ?? 120}px`, '--height': `${height ?? 32}px`} as CSSProperties}
+                />
+            );
+        }
+
+        return placeholder;
+    }, [className, loadingLabel, placeholder, width, height]);
 
     if (error) {
         console.warn('User presented with error', error, typeof error);
