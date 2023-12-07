6392faf6fc6d5e203740bb8247eaf740df0e2bc8
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2023-12-07 01:12:46 +0100

    feat(ui): Popper align, side and offsets

--- web/packages/ui-primitives/src/Popper/Popper.tsx
+++ web/packages/ui-primitives/src/Popper/Popper.tsx
@@ -7,11 +7,13 @@ export type PopperProps = HTMLAttributes<HTMLDivElement> & {
     anchor?: React.ReactNode;
     open?: boolean;
     side?: 'top' | 'right' | 'bottom' | 'left';
+    sideOffset?: number;
     align?: 'start' | 'center' | 'end';
+    alignOffset?: number;
     onOpenChange?: (open: boolean) => void;
 };
 
-export function Popper({ className, trigger, anchor, side, align, open, onOpenChange, ...rest }: PopperProps) {
+export function Popper({ className, trigger, anchor, side, sideOffset, align, alignOffset, open, onOpenChange, ...rest }: PopperProps) {
     return (
         <PopoverPrimitive.Root open={open} onOpenChange={onOpenChange}>
             {trigger && (
@@ -26,10 +28,11 @@ export function Popper({ className, trigger, anchor, side, align, open, onOpenCh
             )}
             <PopoverPrimitive.Portal>
                 <PopoverPrimitive.Content
-                    align="center"
-                    sideOffset={4}
+                    align={align ?? 'center'}
+                    sideOffset={sideOffset ?? 4}
                     side={side}
-                    alignOffset={align === 'center' ? 0 : align === 'start' ? -4 : 4}
+                    alignOffset={alignOffset ?? (align === 'center' ? 0 : align === 'start' ? -4 : 4)}
+                    collisionPadding={Math.max(sideOffset ?? 0, alignOffset ?? 0)}
                     className={cx(
                         'z-50 w-72 rounded-md border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
                         className
