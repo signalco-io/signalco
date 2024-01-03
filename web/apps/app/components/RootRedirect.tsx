0812f7fee3e7e6db8be054a12d06e086d7b58eea
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-01-03 19:52:00 +0100

    fix(app): Root redirect invalid implementation

--- web/apps/app/components/RootRedirect.tsx
+++ web/apps/app/components/RootRedirect.tsx
@@ -1,14 +1,6 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { KnownPages } from '../src/knownPages';
 
 export function RootRedirect() {
-    const router = useRouter();
-    // TODO: Determine what is home page based on user's preferences
-    router.push(KnownPages.Spaces);
-
-    console.debug('Redirecting to root page...');
-
-    return <div></div>;
+    redirect(KnownPages.Spaces);
 }
