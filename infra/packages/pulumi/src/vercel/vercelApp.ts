91a799365b23b8df27835b8bbd2913f6062c2d0e
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-01-18 23:14:35 +0100

    feat(infra): Using GH actions to deploy Vercel Blog app

--- infra/packages/pulumi/src/vercel/vercelApp.ts
+++ infra/packages/pulumi/src/vercel/vercelApp.ts
@@ -8,18 +8,18 @@ export function vercelApp(prefix: string, name: string, {
     framework?: 'nextjs';
     ignoreCommand?: string;
     outputDirectory?: string;
-}) {
+    }, noGit: boolean = false) {
     const project = new Project(`vercel-${prefix}`, {
         framework,
-        gitRepository: {
+        gitRepository: noGit ? undefined : {
             productionBranch: 'main',
             repo: 'signalco-io/signalco',
             type: 'github',
         },
-        buildCommand: `cd ../.. && npx turbo run build --filter=${name}...`,
-        installCommand: `pnpm install --frozen-lockfile --filter ${name}... --filter .`,
-        ignoreCommand,
-        outputDirectory,
+        buildCommand: noGit ? undefined : `cd ../.. && npx turbo run build --filter=${name}...`,
+        installCommand: noGit ? undefined : `pnpm install --frozen-lockfile --filter ${name}... --filter .`,
+        ignoreCommand: noGit ? undefined : ignoreCommand,
+        outputDirectory: noGit ? undefined : outputDirectory,
         name: `signalco-${name}`,
         rootDirectory: `web/apps/${name}`,
         serverlessFunctionRegion: 'dub1',
