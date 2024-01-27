142a3ff29ac6246225e76a55446697443ea451be
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-01-27 02:26:31 +0100

    fix(app): Navigation spaces favorites mobile adjustments and spacing adjustments

--- web/apps/app/components/dashboards/DashboardSelector.tsx
+++ web/apps/app/components/dashboards/DashboardSelector.tsx
@@ -6,6 +6,7 @@ import { Popper } from '@signalco/ui-primitives/Popper';
 import { ButtonDropdown } from '@signalco/ui-primitives/ButtonDropdown';
 import { Button } from '@signalco/ui-primitives/Button';
 import { showNotification } from '@signalco/ui-notifications';
+import { Loadable } from '@signalco/ui/Loadable';
 import { useSearchParam } from '@signalco/hooks/useSearchParam';
 import useDashboards from '../../src/hooks/dashboards/useDashboards';
 import DashboardSelectorMenu from './DashboardSelectorMenu';
@@ -51,31 +52,48 @@ function DashboardSelector({ onEditWidgets, onSettings }: DashboardSelectorProps
 
     return (
         <Row>
-            {(dashboards?.length ?? 0) > 0 && (
-                <Popper
-                    open={isOpen}
-                    onOpenChange={setIsOpen}
-                    trigger={(
-                        <ButtonDropdown className="font-semibold">{currentName}</ButtonDropdown>
-                    )}>
-                    <DashboardSelectorMenu
-                        selectedId={selectedId}
-                        onSelection={handleSelection}
-                        onEditWidgets={handleEditWidgets}
-                        onSettings={onSettings} />
-                </Popper>
-            )}
+            <Loadable
+                isLoading={isLoading}
+                error={error}
+                loadingLabel="Loading spaces..."
+                placeholder="skeletonRect"
+                height={30}
+                width={120}>
+                {(dashboards?.length ?? 0) > 0 && (
+                    <Popper
+                        open={isOpen}
+                        onOpenChange={setIsOpen}
+                        trigger={(
+                            <ButtonDropdown className="w-12 font-semibold sm:w-24 md:w-44">{currentName}</ButtonDropdown>
+                        )}>
+                        <DashboardSelectorMenu
+                            selectedId={selectedId}
+                            onSelection={handleSelection}
+                            onEditWidgets={handleEditWidgets}
+                            onSettings={onSettings} />
+                    </Popper>
+                )}
+            </Loadable>
             {(favoriteDashboards?.length ?? 0) > 0 && (
-                <Row>
-                    {favoriteDashboards?.map(fd => (
-                        <Button
-                            key={fd.id}
-                            variant="plain"
-                            onClick={() => setSelectedId(fd.id)}>
-                            {fd.name}
-                        </Button>
-                    ))}
-                </Row>
+                <div className="relative overflow-x-auto [scroll-timeline:--scroll-timeline_x] [transform:translateZ(0)]">
+                    {/* <div className="fixed inset-y-0 left-0 h-full animate-scroll-overflow [animation-timeline:--scroll-timeline]">
+                        <Left className="opacity-60" />
+                    </div> */}
+                    <Row>
+                        {favoriteDashboards?.map(fd => (
+                            <Button
+                                key={fd.id}
+                                variant="plain"
+                                onClick={() => setSelectedId(fd.id)}
+                                className="px-8 sm:px-5">
+                                {fd.name}
+                            </Button>
+                        ))}
+                    </Row>
+                    {/* <div className="fixed inset-y-0 right-0 h-full animate-scroll-overflow [animation-direction:reverse]">
+                        <Navigate className="opacity-60" />
+                    </div> */}
+                </div>
             )}
         </Row>
     );
--- web/packages/tailwindcss-config-signalco/index.js
+++ web/packages/tailwindcss-config-signalco/index.js
@@ -64,11 +64,16 @@ export const baseConfig = {
                 "accordion-up": {
                     from: { height: "var(--radix-accordion-content-height)" },
                     to: { height: 0 },
+                },
+                "scroll-reveal": {
+                    '0%': { opacity: 0 },
+                    '2%': { opacity: 1 },
                 }
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
+                "scroll-overflow": "scroll-reveal 1ms linear"
             },
         },
     },
