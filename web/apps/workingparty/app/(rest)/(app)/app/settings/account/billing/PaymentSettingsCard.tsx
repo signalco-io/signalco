262a7eb4acb39e6ae89753e086f88af361c6b782
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-03-12 22:46:26 +0100

    chore(workingparty): Better payment method UI

--- web/apps/workingparty/app/(rest)/(app)/app/settings/account/billing/PaymentSettingsCard.tsx
+++ web/apps/workingparty/app/(rest)/(app)/app/settings/account/billing/PaymentSettingsCard.tsx
@@ -1,39 +1,45 @@
 'use client';
 
-import { Typography } from '@signalco/ui-primitives/Typography';
+import { ListItem } from '@signalco/ui-primitives/ListItem';
+import { List } from '@signalco/ui-primitives/List';
 import { NavigatingButton } from '@signalco/ui/NavigatingButton';
 import { Loadable } from '@signalco/ui/Loadable';
-import { camelToSentenceCase } from '@signalco/js';
 import { KnownPages } from '../../../../../../../src/knownPages';
 import { useCurrentUser } from '../../../../../../../src/hooks/data/users/useCurrentUser';
 import { useAccountPaymentMethods } from '../../../../../../../src/hooks/data/account/useAccountPaymentMethods';
 import { SettingsCardActions } from '../../../../../../../src/components/settings/SettingsCardActions';
 import { SettingsCard } from '../../../../../../../src/components/settings/SettingsCard';
+import { PaymentMethodInfo } from './PaymentMethodInfo';
 
 export function PaymentSettingsCard() {
     const currentUser = useCurrentUser();
     const accountId = currentUser.data?.user?.accountIds[0];
     const billing = useAccountPaymentMethods(accountId);
 
-    const defaultPaymentMethod = billing.data?.find(method => method.isDefault);
+    const paymentMethods = billing.data ?? [];
 
     return (
-        <SettingsCard header="Payment Method">
+        <SettingsCard header="Payment Methods">
             <Loadable
                 isLoading={billing.isLoading || currentUser.isLoading}
                 error={currentUser.error || billing.error}
                 loadingLabel="Loading payment methods..."
                 placeholder="skeletonText"
                 width={300}>
-                {defaultPaymentMethod ? (
-                    <Typography level="body1">
-                        Your payment method is <strong>{camelToSentenceCase(defaultPaymentMethod.displayBrand ?? '')}</strong> ending in <strong>****{defaultPaymentMethod.last4}</strong> expiring <strong>{defaultPaymentMethod.expMonth}/{defaultPaymentMethod.expYear}</strong>.
-                    </Typography>
-                ) : (
-                    <Typography level="body1">
-                        No payment method on file.
-                    </Typography>
-                )}
+                <List spacing={1}>
+                    {paymentMethods.map((paymentMethod) => (
+                        <ListItem
+                            key={paymentMethod.id}
+                            className="pl-0"
+                            label={(
+                                <PaymentMethodInfo paymentMethod={paymentMethod} />
+                            )}
+                        />
+                    ))}
+                    {!paymentMethods.length && (
+                        <ListItem label={'No payment method on file.'} />
+                    )}
+                </List>
             </Loadable>
             <SettingsCardActions className="justify-end">
                 <NavigatingButton
