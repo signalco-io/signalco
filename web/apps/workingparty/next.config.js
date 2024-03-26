8dddc7eb2ff1e0552f0eada1f10af2b1221b165f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-03-26 22:14:34 +0100

    Update next.config.js

--- web/apps/workingparty/next.config.js
+++ web/apps/workingparty/next.config.js
@@ -1,7 +1,10 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { createSecureHeaders } from 'next-secure-headers';
 import { combineSecureHeaders, knownSecureHeadersExternalUrls } from '@signalco/data/node';
 import nextBundleAnalyzer from '@next/bundle-analyzer';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 const withBundleAnalyzer = nextBundleAnalyzer({
@@ -11,6 +14,10 @@ const withBundleAnalyzer = nextBundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'standalone',
+    experimental: {
+        outputFileTracingRoot: path.join(__dirname, '../../')
+    },
     images: {
         dangerouslyAllowSVG: true,
         contentSecurityPolicy: 'default-src \'self\'; script-src \'none\'; sandbox;',
