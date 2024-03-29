f98e0de5f7a4ac302323fcce7ba93b6d3f3743e6
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-03-29 01:10:49 +0100

    Update PageNav.tsx

--- web/packages/ui/src/Nav/PageNav.tsx
+++ web/packages/ui/src/Nav/PageNav.tsx
@@ -1,8 +1,8 @@
+import { PropsWithChildren, ReactNode } from 'react';
 import { Row } from '@signalco/ui-primitives/Row';
 import { Link } from '@signalco/ui-primitives/Link';
 import { cx } from '@signalco/ui-primitives/cx';
 import { Container } from '@signalco/ui-primitives/Container';
-import { PropsWithChildren, ReactNode } from 'react';
 
 type PageNavProps = PropsWithChildren<{
     logo: ReactNode;
@@ -15,11 +15,11 @@ export function PageNav({ logo, fullWidth, children }: PageNavProps) {
             'backdrop-blur-md fixed top-0 left-0 right-0 z-10 h-16 border-b flex items-center',
             fullWidth ? 'px-4' : 'px-0'
         )}>
-            <Container maxWidth="lg">
+            <Container>
                 <header>
                     <Row justifyContent="space-between">
                         <div className="flex h-full flex-col items-center">
-                            <Link href='/'>
+                            <Link href="/">
                                 {logo}
                             </Link>
                         </div>
