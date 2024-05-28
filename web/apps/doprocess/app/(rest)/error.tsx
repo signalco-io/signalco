cc1ca261a98454cb501a9e059b0c873c3c3a8f56
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-05-28 14:09:33 +0200

    fix(doprocess): Simplified error and not-found pages

--- web/apps/doprocess/app/(rest)/error.tsx
+++ web/apps/doprocess/app/(rest)/error.tsx
@@ -4,8 +4,10 @@ import { useEffect } from 'react';
 import { Typography } from '@signalco/ui-primitives/Typography';
 import { Stack } from '@signalco/ui-primitives/Stack';
 import { Row } from '@signalco/ui-primitives/Row';
+import { Container } from '@signalco/ui-primitives/Container';
 import { Button } from '@signalco/ui-primitives/Button';
 import { NavigatingButton } from '@signalco/ui/NavigatingButton';
+import { KnownPages } from '../../src/knownPages';
 
 type RootErrorProps = {
     error: Error;
@@ -19,17 +21,15 @@ export default function RootError({ error, reset }: RootErrorProps) {
     }, [error]);
 
     return (
-        <Stack alignItems="center" justifyContent="center" className="mt-16 min-h-fit">
-            <Row justifyContent="center">
-                <Stack spacing={4}>
-                    <Typography level="h1">Page not found</Typography>
-                    <Typography>{'Can\'t find find what you\'re looking for...'}</Typography>
-                    <Row spacing={1}>
-                        <Button variant="plain" onClick={() => reset()}>Try again...</Button>
-                        <NavigatingButton href="/">doprocess home</NavigatingButton>
-                    </Row>
-                </Stack>
-            </Row>
-        </Stack>
+        <Container className="flex h-full items-center" maxWidth="sm">
+            <Stack spacing={4}>
+                <Typography level="h1">Unexpected Error</Typography>
+                <Typography level="body2">We apologize for the inconvenience. Our team is working diligently to resolve this issue. Thank you for your understanding.</Typography>
+                <Row spacing={1}>
+                    <Button variant="plain" onClick={() => reset()}>Try again...</Button>
+                    <NavigatingButton href={KnownPages.Landing}>Home</NavigatingButton>
+                </Row>
+            </Stack>
+        </Container>
     )
 }
