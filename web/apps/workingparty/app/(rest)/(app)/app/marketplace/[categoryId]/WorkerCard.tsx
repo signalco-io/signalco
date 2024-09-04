403fd6fd61de09a4bfc41f1330493b266c26271a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-09-04 02:11:17 +0200

    feat(workingparty): Added search in marketplace

--- web/apps/workingparty/app/(rest)/(app)/app/marketplace/[categoryId]/WorkerCard.tsx
+++ web/apps/workingparty/app/(rest)/(app)/app/marketplace/[categoryId]/WorkerCard.tsx
@@ -4,7 +4,7 @@ import { Fragment } from 'react';
 import { Typography } from '@signalco/ui-primitives/Typography';
 import { Stack } from '@signalco/ui-primitives/Stack';
 import { Row } from '@signalco/ui-primitives/Row';
-import { Card, CardContent, CardTitle } from '@signalco/ui-primitives/Card';
+import { Card, CardContent, CardHeader, CardTitle } from '@signalco/ui-primitives/Card';
 import { Avatar } from '@signalco/ui-primitives/Avatar';
 import { initials } from '@signalco/js';
 import { useSearchParam } from '@signalco/hooks/useSearchParam';
@@ -20,27 +20,29 @@ export function WorkerCard({ worker }: { worker: (typeof marketplaceWorkers)[0];
     };
 
     return (
-        <Card className="border-none bg-transparent" onClick={handleShowWorkerDetails}>
-            <div className="grid grid-cols-[auto_minmax(0,1fr)] grid-rows-[auto_auto] gap-x-2">
-                <Avatar className="bg-foreground text-background">{initials(name)}</Avatar>
-                <CardTitle className="self-center">{name}</CardTitle>
-                <CardContent className="p-0 [grid-column:2] [grid-row:2]">
-                    <Stack spacing={1}>
-                        <Typography level="body1">{description}</Typography>
-                        <Row spacing={1}>
-                            {categories.map((category, categoryIndex) => (
-                                <Fragment key={category}>
-                                    {categoryIndex > 0 && <span className="text-sm opacity-30">•</span>}
-                                    <Typography
-                                        level="body2"
-                                        tertiary
-                                        semiBold>{markeplaceCategoriesFlat.find(c => c.id === category)?.name}</Typography>
-                                </Fragment>
-                            ))}
-                        </Row>
-                    </Stack>
-                </CardContent>
-            </div>
+        <Card onClick={handleShowWorkerDetails}>
+            <CardHeader>
+                <Row spacing={2}>
+                    <Avatar className="">{initials(name)}</Avatar>
+                    <CardTitle className="self-center">{name}</CardTitle>
+                </Row>
+            </CardHeader>
+            <CardContent className="[grid-column:2] [grid-row:2]">
+                <Stack spacing={1}>
+                    <Typography level="body1">{description}</Typography>
+                    <Row spacing={1}>
+                        {categories.map((category, categoryIndex) => (
+                            <Fragment key={category}>
+                                {categoryIndex > 0 && <span className="text-sm opacity-30">•</span>}
+                                <Typography
+                                    level="body3"
+                                    noWrap
+                                    semiBold>{markeplaceCategoriesFlat.find(c => c.id === category)?.name}</Typography>
+                            </Fragment>
+                        ))}
+                    </Row>
+                </Stack>
+            </CardContent>
         </Card>
     );
 }
