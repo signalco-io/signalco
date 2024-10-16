9b428fd3375f6e7204e8fc4d3e1d40615d6bb834
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-10-16 20:38:43 +0200

    feat(ui-primitives): Adjusted Button background colors to be more visible on non-bg colored backgrounds

--- web/packages/ui-primitives/src/Button/Button.tsx
+++ web/packages/ui-primitives/src/Button/Button.tsx
@@ -4,26 +4,29 @@ import { VariantKeys } from '../theme';
 import { Link } from '../Link';
 import { cx } from '../cx'
 
-export type ButtonProps =
-    ({
-        href: string,
-        variant?: VariantKeys | 'link';
-        size?: 'xs' | 'sm' | 'md' | 'lg';
-        startDecorator?: ReactNode;
-        endDecorator?: ReactNode;
-        loading?: boolean;
-        fullWidth?: boolean;
-        disabled?: boolean;
-    } & Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>) | ({
-        href?: never,
-        variant?: VariantKeys | 'link';
-        size?: 'xs' | 'sm' | 'md' | 'lg';
-        startDecorator?: ReactNode;
-        endDecorator?: ReactNode;
-        loading?: boolean;
-        fullWidth?: boolean;
-        disabled?: boolean;
-    } & ButtonHTMLAttributes<HTMLButtonElement>);
+export type ButtonButtonProps = {
+    href?: never,
+    variant?: VariantKeys | 'link';
+    size?: 'xs' | 'sm' | 'md' | 'lg';
+    startDecorator?: ReactNode;
+    endDecorator?: ReactNode;
+    loading?: boolean;
+    fullWidth?: boolean;
+    disabled?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type ButtonLinkProps = {
+    href: string,
+    variant?: VariantKeys | 'link';
+    size?: 'xs' | 'sm' | 'md' | 'lg';
+    startDecorator?: ReactNode;
+    endDecorator?: ReactNode;
+    loading?: boolean;
+    fullWidth?: boolean;
+    disabled?: boolean;
+} & Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>;
+
+export type ButtonProps = ButtonLinkProps | ButtonButtonProps;
 
 const Button = forwardRef<HTMLElement, ButtonProps>(({
     children,
@@ -46,10 +49,10 @@ const Button = forwardRef<HTMLElement, ButtonProps>(({
             ref={ref as any}
             className={cx(
                 'select-none inline-flex gap-1 items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background cursor-default',
-                (!variant || variant === 'soft') && 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+                (!variant || variant === 'soft') && 'bg-primary/15 text-secondary-foreground hover:bg-primary/25',
                 variant === 'outlined' && 'border border-input hover:bg-accent hover:text-accent-foreground',
                 variant === 'plain' && 'hover:bg-accent hover:text-accent-foreground',
-                variant === 'solid' && 'bg-primary text-primary-foreground hover:bg-primary/90',
+                variant === 'solid' && 'bg-primary text-primary-foreground hover:bg-primary/70',
                 (!size || size === 'md') && 'h-10 py-2 px-4',
                 size === 'xs' && 'h-7 px-2 gap-0.5',
                 size === 'sm' && 'h-9 px-3 gap-1',
