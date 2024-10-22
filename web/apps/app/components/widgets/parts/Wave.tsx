59c0f3129fd9a94d335e1b59662374a576a13012
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-10-22 14:47:29 +0200

    chore(dx): Removed module scss in favour of tailwindcss

--- web/apps/app/components/widgets/parts/Wave.tsx
+++ web/apps/app/components/widgets/parts/Wave.tsx
@@ -1,27 +1,38 @@
-import { CSSProperties } from 'react';
+import type { CSSProperties, HTMLAttributes } from 'react';
 import { cx } from '@signalco/ui-primitives/cx';
-import styles from './Wave.module.css';
 
 function SingleWave() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 88.7" className={cx(styles.wave, 'transition-all')}>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 800 88.7"
+            className={cx(
+                'absolute [transform:translate3d(0,0,0)] transition-all w-[200%] h-full',
+                '[&:nth-of-type(1)]:opacity-80 [&:nth-of-type(1)]:animate-wave1',
+                '[&:nth-of-type(2)]:opacity-50 [&:nth-of-type(2)]:bottom-0 [&:nth-of-type(2)]:animate-wave2',
+                '[&:nth-of-type(3)]:opacity-50 [&:nth-of-type(3)]:bottom-0 [&:nth-of-type(3)]:animate-wave3',
+            )}>
             <path d="M800 56.9c-155.5 0-204.9-50-405.5-49.9-200 0-250 49.9-394.5 49.9v31.8h800v-.2-31.6z" />
         </svg>
     )
 }
 
-export function Wave({ value, breakpoints }: { value: number, breakpoints: readonly [number, number, number] }) {
+export type WaveProps = HTMLAttributes<HTMLDivElement> & {
+    value: number,
+    breakpoints: readonly [number, number, number]
+};
+
+export function Wave({ value, breakpoints, ...rest }: WaveProps) {
     return (
-        <div>
+        <div {...rest}>
             <div
                 className={cx(
-                    styles.root,
                     'transition-all',
+                    'absolute w-full h-5 left-0 right-0 bottom-[--wave-height] overflow-x-hidden',
                     value < breakpoints[0] && 'fill-green-500',
                     value >= breakpoints[0] && value < breakpoints[1] && 'fill-lime-500 dark:fill-lime-600',
                     value >= breakpoints[1] && value < breakpoints[2] && 'fill-amber-400 dark:fill-amber-600',
                     value >= breakpoints[2] && 'fill-rose-500 dark:fill-rose-700',
-                    // 'fill-red-400'
                 )}
                 style={{
                     '--wave-height': `${value}%`,
@@ -31,8 +42,9 @@ export function Wave({ value, breakpoints }: { value: number, breakpoints: reado
                 <SingleWave />
             </div>
             <div
-                className={cx(styles.background,
+                className={cx(
                     'transition-all',
+                    'absolute w-full h-[calc(var(--wave-height)+7px)] left-0 right-0 bottom-0 overflow-x-hidden',
                     value < breakpoints[0] && 'bg-green-400 dark:bg-green-500',
                     value >= breakpoints[0] && value < breakpoints[1] && 'bg-lime-400 dark:bg-lime-600',
                     value >= breakpoints[1] && value < breakpoints[2] && 'bg-amber-400 dark:bg-amber-600',
