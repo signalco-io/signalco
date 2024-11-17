c33b873f0c3ecc65b8622cbe079d93dbd8f5beb8
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-11-17 13:46:10 +0100

    feat(cloud): Added entities delete modern endpoint

--- cloud/src/Signalco.Api.Public/Functions/Entity/EntityDeleteFunction.cs
+++ cloud/src/Signalco.Api.Public/Functions/Entity/EntityDeleteFunction.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.OpenApi.Models;
 using Signal.Api.Common.Auth;
 using Signal.Api.Common.Exceptions;
 using Signal.Api.Common.OpenApi;
@@ -18,6 +19,7 @@ public class EntityDeleteFunction(
     IEntityService entityService)
 {
     [Function("Entity-Delete")]
+    [Obsolete]
     [OpenApiSecurityAuth0Token]
     [OpenApiOperation<EntityDeleteFunction>("Entity", Description = "Deletes the entity.")]
     [OpenApiJsonRequestBody<EntityDeleteDto>(Description = "Information about entity to delete.")]
@@ -36,6 +38,29 @@ public class EntityDeleteFunction(
             await entityService.RemoveAsync(
                 context.User.UserId,
                 context.Payload.Id,
+                cancellationToken);
+        });
+
+    [Function("Entities-Delete")]
+    [OpenApiSecurityAuth0Token]
+    [OpenApiOperation<EntityDeleteFunction>("Entities", Description = "Deletes the entity.")]
+    [OpenApiParameter("id", In = ParameterLocation.Path, Required = true, Type = typeof(Guid), Description = "Entity identifier")]
+    [OpenApiResponseBadRequestValidation]
+    [OpenApiResponseWithoutBody]
+    [OpenApiResponseWithoutBody(HttpStatusCode.NotFound)]
+    public async Task<HttpResponseData> RunDeleteSingle(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "entities")]
+        HttpRequestData req,
+        string? id,
+        CancellationToken cancellationToken = default) =>
+        await req.UserRequest(cancellationToken, functionAuthenticator, async context =>
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ExpectedHttpException(HttpStatusCode.BadRequest, "Id property is required.");
+
+            await entityService.RemoveAsync(
+                context.User.UserId,
+                id,
                 cancellationToken);
         });
 
