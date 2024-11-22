a8ff2519280d0481a72f0cf3aeea8ad428444c1b
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-11-22 19:02:58 +0100

    fix(auth-server): Export missing types

--- web/packages/auth-server/src/index.ts
+++ web/packages/auth-server/src/index.ts
@@ -1,2 +1,7 @@
 export * from './InitAuth';
+
+// Type exports
 export type { WithAuthContext } from './withAuth';
+export type { UserBase } from './@types/UserBase';
+export type { AuthContext } from './@types/AuthContext';
+export type { AuthConfig } from './@types/AuthConfig';
