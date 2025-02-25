06c19393dc60c0d9ceac4636ac5827789355be09
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-02-25 23:27:32 +0100

    fix(auth-server): Override type not being full partial

--- web/packages/auth-server/src/@types/AuthConfigOverride.ts
+++
@@ -1,8 +0,0 @@
-import type { UserBase } from './UserBase';
-import type { AuthConfig } from './AuthConfig';
-
-type DeepPartial<T> = {
-    [K in keyof T]: T[K] extends Partial<T[K]> ? T[K] : DeepPartial<T[K]>
-};
-
-export type AuthConfigOverride<TUser extends UserBase> = DeepPartial<AuthConfig<TUser>>;
