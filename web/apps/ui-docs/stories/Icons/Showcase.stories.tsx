bbb28a9c94553194806a9b5b5227171b59b1d6e2
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-05-04 12:24:40 +0200

    feat(ui-icons): Added new set of icons

---
+++ web/apps/ui-docs/stories/Icons/Showcase.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import * as Icons from "@signalco/ui-icons"
+
+const meta: Meta =  { 
+    tags: ['autodocs']
+};
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+    render: () => {
+        return (
+            <div className="grid grid-cols-10 gap-4">
+                {Object.entries(Icons).map(([name, Icon]) => {
+                    const Component = Icon as React.FC<React.SVGProps<SVGSVGElement>>;
+                    return (
+                        <div key={name} className="flex flex-col items-center justify-center gap-2">
+                            <Component className="h-8 w-8" />
+                            <span className="text-xs">{name}</span>
+                        </div>
+                    );
+                }
+            )}
+            </div>
+        );
+    }
+};
